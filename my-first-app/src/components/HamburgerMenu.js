9000fc1a31bfc52fc4a02d65603e4f0888a8c118
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-06 20:16:55 +0100

    changes

--- my-first-app/src/components/HamburgerMenu.js
+++ my-first-app/src/components/HamburgerMenu.js
@@ -3,7 +3,7 @@ import classes from '../style/HamburgerStyleCSSModule.module.css'
 
 export const HamburgerMenu = () => {
   return (
-    <button className={classes.hamburger}>
+    <button className={`${classes.hamburger} ${classes.hamburgeractive}`}>
         <span className={classes.hamburger__box}>
             <span className={classes.hamburger__inner}></span>
         </span>
