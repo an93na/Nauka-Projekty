0c4d85bd8543fc0c50e9601e4a7744a78621cf0e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-07 19:33:13 +0100

    changes

--- my-first-app/src/components/HamburgerMenu.js
+++ my-first-app/src/components/HamburgerMenu.js
@@ -6,14 +6,8 @@ const lineItemStyle = { listStyle: "none" };
 
 export const HamburgerMenu = () => {
   const [open, setOpen] = useState(false);
-  const handleCloseClick = () => [
-    setOpen(false)
-  ]
-  return (
-    <div className={classes.hamburger}>
-      <span className={classes.hamburger__box}>
-        <span className={classes.hamburger__inner}></span>
-      </span>
-    </div>
-  );
+  const handleCloseClick = () => [setOpen(false)];
+  return <div className={classes.hamburger}>
+    
+  </div>;
 };
