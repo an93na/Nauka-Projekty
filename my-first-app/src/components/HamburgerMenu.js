60ffbf1d4fd9ba68f568774795e10dbfea4fa90c
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-07 19:28:51 +0100

    changes

--- my-first-app/src/components/HamburgerMenu.js
+++ my-first-app/src/components/HamburgerMenu.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import classes from '../style/HamburgerStyleCSSModule.module.css'
-import Burger from "./hamb.png"
+import React, { useState } from "react";
+import classes from "../style/HamburgerStyleCSSModule.module.css";
+import Burger from "./hamb.png";
+
+const lineItemStyle = { listStyle: "none" };
 
 export const HamburgerMenu = () => {
-    let hamburger = classes.hamburger
+  const [open, setOpen] = useState(false);
   return (
     <button>
-        <span className={classes.hamburger__box}>
-            <span className={classes.hamburger__inner}></span>
-        </span>
+      <span className={classes.hamburger__box}>
+        <span className={classes.hamburger__inner}></span>
+      </span>
     </button>
-  )
-}
+  );
+};
