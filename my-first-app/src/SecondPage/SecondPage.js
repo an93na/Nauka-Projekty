73ad7e55cb5c6db4098b17bab3e298d20bc8d4da
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-08 20:50:53 +0100

    changes

--- my-first-app/src/SecondPage/SecondPage.js
+++ my-first-app/src/SecondPage/SecondPage.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import {classes} from '../style/SecondPageCSS.module.css'
 
 export const SecondPage = () => {
   return (
-    <article>SecondPage</article>
-  )
-}
+    <article>
+      <h3>SecondPage</h3>
+    </article>
+  );
+};
