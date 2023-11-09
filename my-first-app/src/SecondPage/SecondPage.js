f27847415bd006d42c663e1f5aec77eb6d3b76d9
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-09 19:46:35 +0100

    changes

--- my-first-app/src/SecondPage/SecondPage.js
+++ my-first-app/src/SecondPage/SecondPage.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import classes from "../style/SecondPageCSS.module.css";
 
 export const SecondPage = () => {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState([]);
   const [priority, setPriority] = useState("");
   return (
     <article>
       <h3>Dodaj zadanie</h3>
-      <form action="">
+      <form action="" onSubmit={(e) => {
+        e.preventDefault()
+
+      }}>
         <input
           type="text"
           name="zadanie"
@@ -16,11 +19,19 @@ export const SecondPage = () => {
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <select name="zadanie" id="">
+        <select
+          name="zadanie"
+          id=""
+          onChange={(e) => setPriority(e.target.value)}
+        >
           <option value="" hidden>
             Wybierz priorytet
           </option>
+          <option value="hight">hight</option>
+          <option value="medium">medium</option>
+          <option value="low">low</option>
         </select>
+        <button type="submit">Submit</button>
       </form>
     </article>
   );
