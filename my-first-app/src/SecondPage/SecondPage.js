3b8acde08c7855f45a5d30f0c404c8ef39e7a67e
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-09 19:44:04 +0100

    changes

--- my-first-app/src/SecondPage/SecondPage.js
+++ my-first-app/src/SecondPage/SecondPage.js
@@ -1,10 +1,27 @@
-import React from "react";
-import classes from '../style/SecondPageCSS.module.css'
+import React, { useState } from "react";
+import classes from "../style/SecondPageCSS.module.css";
 
 export const SecondPage = () => {
+  const [todo, setTodo] = useState("");
+  const [priority, setPriority] = useState("");
   return (
     <article>
-      <h3>SecondPage</h3>
+      <h3>Dodaj zadanie</h3>
+      <form action="">
+        <input
+          type="text"
+          name="zadanie"
+          id=""
+          placeholder="zadanie"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <select name="zadanie" id="">
+          <option value="" hidden>
+            Wybierz priorytet
+          </option>
+        </select>
+      </form>
     </article>
   );
 };
