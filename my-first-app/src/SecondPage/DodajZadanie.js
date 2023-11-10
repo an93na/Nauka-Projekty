c21c4d62820f3de54d639cd0aa6c9aa0cb68ee52
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-10 19:54:16 +0100

    changes

---
+++ my-first-app/src/SecondPage/DodajZadanie.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Zadania } from "./Zadania";
+
+export const DodajZadanie = () => {
+  const [todo, setTodo] = useState("");
+  const [priority, setPriority] = useState("");
+  const [tabTodo, setTabTodo] = useState([]);
+  return (
+    <div>
+      <h3>Dodaj zadanie</h3>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newTab = { todo, priority, id: Math.random() };
+          setTabTodo((prev) => [...prev, newTab]);
+          setTodo("");
+          setPriority("");
+        }}
+      >
+        <input
+          type="text"
+          name="zadanie"
+          id=""
+          placeholder="zadanie"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          required
+        />
+        <select
+          name="zadanie"
+          id=""
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          required
+        >
+          <option value="" hidden>
+            Wybierz priorytet
+          </option>
+          <option value="hight">hight</option>
+          <option value="medium">medium</option>
+          <option value="low">low</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+      <Zadania tabTodo={tabTodo} />
+    </div>
+  );
+};
--- my-first-app/src/SecondPage/SecondPage.js
+++ my-first-app/src/SecondPage/SecondPage.js
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
 import classes from "../style/SecondPageCSS.module.css";
+import { Link, Route, Routes } from "react-router-dom";
+import { DodajZadanie } from "./DodajZadanie";
 import { Zadania } from "./Zadania";
-import { Link } from "react-router-dom";
 
 export const SecondPage = () => {
-  const [todo, setTodo] = useState("");
-  const [priority, setPriority] = useState("");
-  const [tabTodo, setTabTodo] = useState([]);
   return (
     <article>
       <div className={classes.nawi}>
@@ -14,43 +11,11 @@ export const SecondPage = () => {
         <Link to="/add-task">Dodaj zadanie</Link>
         <Link to="/lista">Lista</Link>
       </div>
-      <h3>Dodaj zadanie</h3>
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          const newTab = { todo, priority, id: Math.random() };
-          setTabTodo((prev) => [...prev, newTab]);
-          setTodo("");
-          setPriority("");
-        }}
-      >
-        <input
-          type="text"
-          name="zadanie"
-          id=""
-          placeholder="zadanie"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-          required
-        />
-        <select
-          name="zadanie"
-          id=""
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
-          required
-        >
-          <option value="" hidden>
-            Wybierz priorytet
-          </option>
-          <option value="hight">hight</option>
-          <option value="medium">medium</option>
-          <option value="low">low</option>
-        </select>
-        <button type="submit">Submit</button>
-      </form>
-      <Zadania tabTodo={tabTodo} />
+      <Routes>
+        <Route path="/second-page" element={<SecondPage/>}/>
+        <Route path="/add-task" element={<DodajZadanie/>}/>
+        <Route path="/lista" element={<Zadania/>}/>
+      </Routes>
     </article>
   );
 };
