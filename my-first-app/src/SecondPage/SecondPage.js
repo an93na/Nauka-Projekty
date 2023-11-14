f85a9224d5abf27c55761aa729d25f6b4a56b3c8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-14 17:49:15 +0100

    changes

--- my-first-app/src/SecondPage/SecondPage.js
+++ my-first-app/src/SecondPage/SecondPage.js
@@ -10,10 +10,12 @@ export const SecondPage = () => {
   return (
     <article>
       <div className={classes.nawi}>
+        <Link to="/second-page">Home</Link>
         <Link to="add-task">Dodaj zadanie</Link>
         <Link to="lista">Lista</Link>
       </div>
       <Routes>
+        <Route path="/second-page" element={<SecondPage/>}/>
         <Route
           path="add-task"
           element={<DodajZadanie setTabTodo={setTabTodo} />}
