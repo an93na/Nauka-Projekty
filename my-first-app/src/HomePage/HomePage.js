a578592bf56ba533c379cfb501d39df3023cbf20
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-15 19:15:21 +0100

    changes

--- my-first-app/src/HomePage/HomePage.js
+++ my-first-app/src/HomePage/HomePage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const HomePage = () => {
   return (
-    <article>HomePage</article>
-  )
-}
+    <article>
+      <h3>HomePage</h3>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quidem officiis quis! Iusto, hic, saepe sunt maxime nesciunt maiores quasi qui quisquam amet tempora iure distinctio, harum ipsa molestiae! Nemo.</p>
+    </article>
+  );
+};
