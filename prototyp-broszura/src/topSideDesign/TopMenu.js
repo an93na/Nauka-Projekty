d6cc1f2ddc9c1ef4841c16a93a54291f69649cde
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-16 20:32:55 +0100

    changes

--- prototyp-broszura/src/topSideDesign/TopMenu.js
+++ prototyp-broszura/src/topSideDesign/TopMenu.js
@@ -5,10 +5,26 @@ export const TopMenu = () => {
   return (
     <div style={{backgroundColor: 'deeppink', padding:10, margin: 5}}>
         <h3 style={{marginLeft:40}}>Czytaj bo tego potrzebujesz</h3>
-        <div style={{display: 'flex'}}>
-            <div style={{display: 'flex'}}>
-                <img src={kartki} alt="kartki" style={{width: '30px', marginLeft: 40}} />
-                <p style={{textAlign:'center'}}>kartki</p>
+        <div style={{display: 'flex', gap: 10}}>
+            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px', cursor: 'pointer'}}>
+                <img src={kartki} alt="kartki" style={{width: '20px'}} />
+                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+            </div>
+            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+                <img src={kartki} alt="kartki" style={{width: '20px'}} />
+                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+            </div>
+            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+                <img src={kartki} alt="kartki" style={{width: '20px'}} />
+                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+            </div>
+            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+                <img src={kartki} alt="kartki" style={{width: '20px'}} />
+                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+            </div>
+            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+                <img src={kartki} alt="kartki" style={{width: '20px'}} />
+                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
             </div>
         </div>
     </div>
