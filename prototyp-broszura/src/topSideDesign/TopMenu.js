c9998fc74796397f458a78850a649ecf86e3b33a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-16 20:25:18 +0100

    changes

--- prototyp-broszura/src/topSideDesign/TopMenu.js
+++ prototyp-broszura/src/topSideDesign/TopMenu.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import kartki from './iconmonstr-layer-multiple-lined-240.png'
 
 export const TopMenu = () => {
   return (
-    <div style={{backgroundColor: 'gray', padding:10, margin: 5}}>
-        <h3>Czytaj bo tego potrzebujesz</h3>
+    <div style={{backgroundColor: 'deeppink', padding:10, margin: 5}}>
+        <h3 style={{marginLeft:40}}>Czytaj bo tego potrzebujesz</h3>
+        <div style={{display: 'flex'}}>
+            <div style={{display: 'flex'}}>
+                <img src={kartki} alt="kartki" style={{width: '30px', marginLeft: 40}} />
+                <p style={{textAlign:'center'}}>kartki</p>
+            </div>
+        </div>
     </div>
   )
 }
