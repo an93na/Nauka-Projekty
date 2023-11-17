5b3263beef3b94690b9c36b4ac65567ca9a085c7
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-17 20:14:21 +0100

    changes

--- prototyp-broszura/src/topSideDesign/TopMenu.js
+++ prototyp-broszura/src/topSideDesign/TopMenu.js
@@ -3,28 +3,28 @@ import kartki from './iconmonstr-layer-multiple-lined-240.png'
 
 export const TopMenu = () => {
   return (
-    <div style={{backgroundColor: 'deeppink', padding:10, margin: 5}}>
-        <h3 style={{marginLeft:40}}>Czytaj bo tego potrzebujesz</h3>
-        <div style={{display: 'flex', gap: 10}}>
-            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px', cursor: 'pointer'}}>
+    <div style={{backgroundColor: 'deepskyblue', padding:10, margin: 5}}>
+        <h3 style={{marginLeft:40, color: 'black'}}>Czytaj bo tego potrzebujesz</h3>
+        <div style={{display: 'flex', gap: 20, flexDirection: 'row', flexWrap: 'wrap', marginLeft: '40px'}}>
+            <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px', cursor: 'pointer'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
-                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+                <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
             </div>
-            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+            <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
-                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+                <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
             </div>
-            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+            <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
-                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+                <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
             </div>
-            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+            <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
-                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+                <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
             </div>
-            <div style={{display: 'flex', border: '1px solid white', padding: 10, borderRadius: '10px'}}>
+            <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
-                <p style={{textAlign:'center', margin: 'auto'}}>kartki</p>
+                <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
             </div>
         </div>
     </div>
