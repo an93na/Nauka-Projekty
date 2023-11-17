8ea6391a64a2ef2b1186d0f78e7aa0a2edf4e7dd
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-17 20:16:49 +0100

    changes

--- prototyp-broszura/src/topSideDesign/TopMenu.js
+++ prototyp-broszura/src/topSideDesign/TopMenu.js
@@ -4,8 +4,8 @@ import kartki from './iconmonstr-layer-multiple-lined-240.png'
 export const TopMenu = () => {
   return (
     <div style={{backgroundColor: 'deepskyblue', padding:10, margin: 5}}>
-        <h3 style={{marginLeft:40, color: 'black'}}>Czytaj bo tego potrzebujesz</h3>
-        <div style={{display: 'flex', gap: 20, flexDirection: 'row', flexWrap: 'wrap', marginLeft: '40px'}}>
+        <h3 style={{marginLeft:40, marginBottom: 20, color: 'black'}}>Czytaj bo tego potrzebujesz</h3>
+        <div style={{display: 'flex', gap: 20, flexDirection: 'row', flexWrap: 'wrap', marginLeft: '40px', marginTop: 0}}>
             <div style={{display: 'flex', border: '1px solid black', padding: 10, borderRadius: '10px', cursor: 'pointer'}}>
                 <img src={kartki} alt="kartki" style={{width: '20px'}} />
                 <p style={{textAlign:'center', margin: 'auto', color: 'black'}}>kartki</p>
