ebda8cdd47f18416ccec97a4d1034170875101bb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-22 21:20:39 +0100

    changes

--- prototyp-broszura/src/SiteDesign/DodajPost.js
+++ prototyp-broszura/src/SiteDesign/DodajPost.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const DodajPost = (props) => {
   const { posts, setPosts } = props;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const liczba = posts.length+1
+  const [lp, setLp] = useState(liczba)
+  // console.log(lp)
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        setLp(lp+1)
+        const newPost = {
+          id: lp,
+          title,
+          content,
+        };
+        setPosts((prev) => [...prev, newPost]);
+        setContent("");
+        setTitle("");
       }}
     >
-      <input type="text" placeholder="title" required/>
-      <input type="text" placeholder="content" required/>
+      <input
+        type="text"
+        placeholder="title"
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="content"
+        required
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
       <button type="submit">Dodaj</button>
     </form>
   );
