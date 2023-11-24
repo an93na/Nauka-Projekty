678684c3656796057cf15a56391a9e3fc59119e5
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-24 19:28:12 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszury.js
+++ prototyp-broszura/src/SiteDesign/Broszury.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Broszury = () => {
+  const [klik, setKlik] = useState(false);
   return (
-    <article>Broszury</article>
-  )
-}
+    <article
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button
+        style={{
+          width: 300,
+          backgroundColor: "palevioletred",
+          padding: 20,
+          textAlign: "center",
+          borderRadius: "10px",
+        }}
+        value={klik}
+        onClick={(prev) => setKlik(!prev)}
+      >
+        Broszury
+      </button>
+    </article>
+  );
+};
