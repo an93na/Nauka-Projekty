877a81ba5145eb444d527341f7a6a22f85b2066d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-24 20:16:42 +0100

    changes

--- prototyp-broszura/src/Navigation/Navigation.js
+++ prototyp-broszura/src/Navigation/Navigation.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { SiteDesign } from '../SiteDesign/SiteDesign'
-import { Books } from '../SiteDesign/Books'
-import { Broszury } from '../SiteDesign/Broszury'
-import { Czasopisma } from '../SiteDesign/Czasopisma'
-import { Kartki } from '../SiteDesign/Kartki'
-import { NotFound } from '../SiteDesign/NotFound'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { SiteDesign } from "../SiteDesign/SiteDesign";
+import { Books } from "../SiteDesign/Books";
+import { Broszury } from "../SiteDesign/Broszura/Broszury";
+import { Czasopisma } from "../SiteDesign/Czasopisma";
+import { Kartki } from "../SiteDesign/Kartki";
+import { NotFound } from "../SiteDesign/NotFound";
 
 export const Navigation = () => {
   return (
     <Routes>
-        <Route path='/' element={<SiteDesign/>}/>
-        <Route path='/books' element={<Books/>}/>
-        <Route path='/broszury' element={<Broszury/>}/>
-        <Route path='/czasopisma' element={<Czasopisma/>}/>
-        <Route path='/kartki' element={<Kartki/>}/>
-        <Route path='*' element={<NotFound/>}/>
+      <Route path="/" element={<SiteDesign />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/broszury" element={<Broszury />} />
+      <Route path="/czasopisma" element={<Czasopisma />} />
+      <Route path="/kartki" element={<Kartki />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
-}
+  );
+};
