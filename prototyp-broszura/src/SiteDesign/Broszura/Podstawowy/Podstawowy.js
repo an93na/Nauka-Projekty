f2d0152a65f70d256814e4d6e3afd3944465bb34
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-25 16:34:45 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Podstawowy.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Podstawowy.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import { Opis } from "./Opis";
+import { Przykład } from "./Przykład";
+import { Zadnie } from "./Zadnie";
 
 export const Podstawowy = () => {
-  const [Wybore, setWybor]=useState('')
+  const [wybor, setWybor] = useState("");
   return (
     <div>
       <h3 style={{ color: "green" }}>Poziom podstawowy</h3>
-      <select>
-        <option value="" hidden>Wybierz z listy</option>
+      <select value={wybor} onChange={(e) => setWybor(e.target.value)}>
+        <option value="" hidden>
+          Wybierz z listy
+        </option>
         <option value="opis">Opis</option>
         <option value="przyklad">Przykład</option>
         <option value="zadanie">Zadanie</option>
       </select>
-      {}
+      {wybor === "opis" && <Opis />}
+      {wybor === "przyklad" && <Przykład />}
+      {wybor === "zadanie" && <Zadnie />}
     </div>
   );
 };
