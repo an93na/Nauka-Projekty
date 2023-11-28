82960862ff59050b1546c9f332c51dde7895b6ea
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-28 21:19:30 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
@@ -4,7 +4,7 @@ export const Zadnie = () => {
   return (
     <div>
       <h5>Zadanie</h5>
-      <p style={{marginBottom: 2, marginTop: 4}}>
+      <p style={{ marginBottom: 2, marginTop: 4 }}>
         1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
       </p>
       <label>
@@ -19,7 +19,7 @@ export const Zadnie = () => {
         <input type="radio" name="pyt1" id="1c" />
         Lorem, ipsum dolor.
       </label>
-      <p style={{marginBottom: 2, marginTop: 10}}>
+      <p style={{ marginBottom: 2, marginTop: 10 }}>
         2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
       </p>
       <label>
@@ -32,6 +32,36 @@ export const Zadnie = () => {
       </label>
       <label>
         <input type="radio" name="pyt2" id="1c" />
+        Lorem, ipsum dolor.
+      </label>
+      <p style={{ marginBottom: 2, marginTop: 10 }}>
+        3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
+      </p>
+      <label>
+        <input type="radio" name="pyt3" id="1a" />
+        Lorem ipsum dolor sit amet.
+      </label>
+      <label>
+        <input type="radio" name="pyt3" id="1b" />
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </label>
+      <label>
+        <input type="radio" name="pyt3" id="1c" />
+        Lorem, ipsum dolor.
+      </label>
+      <p style={{ marginBottom: 2, marginTop: 10 }}>
+        4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
+      </p>
+      <label>
+        <input type="radio" name="pyt4" id="1a" />
+        Lorem ipsum dolor sit amet.
+      </label>
+      <label>
+        <input type="radio" name="pyt4" id="1b" />
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </label>
+      <label>
+        <input type="radio" name="pyt4" id="1c" />
         Lorem, ipsum dolor.
       </label>
     </div>
