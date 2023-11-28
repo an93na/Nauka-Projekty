02e16a801ef3e8c88ef9f1a201f3246b7a02b38b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-28 21:18:05 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export const Zadnie = () => {
   return (
     <div>
       <h5>Zadanie</h5>
-      <p>1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?</p>
+      <p style={{marginBottom: 2, marginTop: 4}}>
+        1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
+      </p>
       <label>
-      <input type="radio" name="pyt1" id="1a" />
+        <input type="radio" name="pyt1" id="1a" />
         Lorem ipsum dolor sit amet.
       </label>
       <label>
-      <input type="radio" name="pyt1" id="1b" />
+        <input type="radio" name="pyt1" id="1b" />
         Lorem ipsum dolor sit, amet consectetur adipisicing.
       </label>
       <label>
-      <input type="radio" name="pyt1" id="1c" />
+        <input type="radio" name="pyt1" id="1c" />
+        Lorem, ipsum dolor.
+      </label>
+      <p style={{marginBottom: 2, marginTop: 10}}>
+        2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
+      </p>
+      <label>
+        <input type="radio" name="pyt2" id="1a" />
+        Lorem ipsum dolor sit amet.
+      </label>
+      <label>
+        <input type="radio" name="pyt2" id="1b" />
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </label>
+      <label>
+        <input type="radio" name="pyt2" id="1c" />
         Lorem, ipsum dolor.
       </label>
     </div>
-  )
-}
+  );
+};
