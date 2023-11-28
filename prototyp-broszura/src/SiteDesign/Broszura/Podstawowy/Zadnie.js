834327cab45a33b5cbf770d36cdf1fd4938c3adc
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-28 21:07:07 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const Zadnie = () => {
   return (
-    <div>Zadnie</div>
+    <div>
+      <h5>Zadanie</h5>
+      <label>
+      <input type="radio" name="pyt1" id="" />
+        pyt1a
+      </label>
+      <label>
+      <input type="radio" name="pyt1" id="" />
+        pyt1b
+      </label>
+      <label>
+      <input type="radio" name="pyt1" id="" />
+        pyt1c
+      </label>
+    </div>
   )
 }
