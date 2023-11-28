2cd0cd7fbce55a67b56ad55c959ff437f62a0451
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-28 21:14:22 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
@@ -4,17 +4,18 @@ export const Zadnie = () => {
   return (
     <div>
       <h5>Zadanie</h5>
+      <p>1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?</p>
       <label>
-      <input type="radio" name="pyt1" id="" />
-        pyt1a
+      <input type="radio" name="pyt1" id="1a" />
+        Lorem ipsum dolor sit amet.
       </label>
       <label>
-      <input type="radio" name="pyt1" id="" />
-        pyt1b
+      <input type="radio" name="pyt1" id="1b" />
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
       </label>
       <label>
-      <input type="radio" name="pyt1" id="" />
-        pyt1c
+      <input type="radio" name="pyt1" id="1c" />
+        Lorem, ipsum dolor.
       </label>
     </div>
   )
