82220b3ffb8f9c8067f3d27045b7880b59b776e8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-29 20:50:37 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Podstawowy/Zadnie.js
@@ -5,7 +5,10 @@ export const Zadnie = () => {
   const [two, setTwo] = useState("");
   const [three, setThree] = useState("");
   const [four, setFour] = useState("");
-  console.log(one)
+  console.log(one);
+  console.log("two", two);
+  console.log("three", three);
+  console.log("four", four);
   return (
     <div>
       <h5>Zadanie</h5>
@@ -19,60 +22,132 @@ export const Zadnie = () => {
           1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
         </p>
         <label>
-          <input type="radio" name="pyt1" id="1a" value={'a'} onChange={(e) => setOne(e.target.value)} />
+          <input
+            type="radio"
+            name="pyt1"
+            id="1a"
+            value={"a"}
+            onChange={(e) => setOne(e.target.value)}
+          />
           Lorem ipsum dolor sit amet.
         </label>
         <label>
-          <input type="radio" name="pyt1" id="1b" value={'b'} onChange={(e) => setOne(e.target.value)}/>
+          <input
+            type="radio"
+            name="pyt1"
+            id="1b"
+            value={"b"}
+            onChange={(e) => setOne(e.target.value)}
+          />
           Lorem ipsum dolor sit, amet consectetur adipisicing.
         </label>
         <label>
-          <input type="radio" name="pyt1" id="1c" value={'c'} onChange={(e) => setOne(e.target.value)}/>
+          <input
+            type="radio"
+            name="pyt1"
+            id="1c"
+            value={"c"}
+            onChange={(e) => setOne(e.target.value)}
+          />
           Lorem, ipsum dolor.
         </label>
         <p style={{ marginBottom: 2, marginTop: 10 }}>
           2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
         </p>
         <label>
-          <input type="radio" name="pyt2" id="1a" />
+          <input
+            type="radio"
+            name="pyt2"
+            id="2a"
+            value={"a"}
+            onChange={(e) => setTwo(e.target.value)}
+          />
           Lorem ipsum dolor sit amet.
         </label>
         <label>
-          <input type="radio" name="pyt2" id="1b" />
+          <input
+            type="radio"
+            name="pyt2"
+            id="2b"
+            value={"b"}
+            onChange={(e) => setTwo(e.target.value)}
+          />
           Lorem ipsum dolor sit, amet consectetur adipisicing.
         </label>
         <label>
-          <input type="radio" name="pyt2" id="1c" />
+          <input
+            type="radio"
+            name="pyt2"
+            id="2c"
+            value={"c"}
+            onChange={(e) => setTwo(e.target.value)}
+          />
           Lorem, ipsum dolor.
         </label>
         <p style={{ marginBottom: 2, marginTop: 10 }}>
           3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
         </p>
         <label>
-          <input type="radio" name="pyt3" id="1a" />
+          <input
+            type="radio"
+            name="pyt3"
+            id="3a"
+            value={"a"}
+            onChange={(e) => setThree(e.target.value)}
+          />
           Lorem ipsum dolor sit amet.
         </label>
         <label>
-          <input type="radio" name="pyt3" id="1b" />
+          <input
+            type="radio"
+            name="pyt3"
+            id="3b"
+            value={"b"}
+            onChange={(e) => setThree(e.target.value)}
+          />
           Lorem ipsum dolor sit, amet consectetur adipisicing.
         </label>
         <label>
-          <input type="radio" name="pyt3" id="1c" />
+          <input
+            type="radio"
+            name="pyt3"
+            id="3c"
+            value={"c"}
+            onChange={(e) => setThree(e.target.value)}
+          />
           Lorem, ipsum dolor.
         </label>
         <p style={{ marginBottom: 2, marginTop: 10 }}>
           4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, est?
         </p>
         <label>
-          <input type="radio" name="pyt4" id="1a" />
+          <input
+            type="radio"
+            name="pyt4"
+            id="4a"
+            value={"a"}
+            onChange={(e) => setFour(e.target.value)}
+          />
           Lorem ipsum dolor sit amet.
         </label>
         <label>
-          <input type="radio" name="pyt4" id="1b" />
+          <input
+            type="radio"
+            name="pyt4"
+            id="4b"
+            value={"b"}
+            onChange={(e) => setFour(e.target.value)}
+          />
           Lorem ipsum dolor sit, amet consectetur adipisicing.
         </label>
         <label>
-          <input type="radio" name="pyt4" id="1c" />
+          <input
+            type="radio"
+            name="pyt4"
+            id="4c"
+            value={"c"}
+            onChange={(e) => setFour(e.target.value)}
+          />
           Lorem, ipsum dolor.
         </label>
         <button style={{ marginTop: "20px", backgroundColor: "green" }}>
