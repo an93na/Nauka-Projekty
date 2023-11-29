28708b161d9c485f213381c66e502e6d8851138f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-11-29 20:35:50 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Broszura/SrednioZaawansowany/SrednioZaawansowany.js
+++ prototyp-broszura/src/SiteDesign/Broszura/SrednioZaawansowany/SrednioZaawansowany.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Zadnie } from "./Zadanie";
 
 export const SrednioZaawansowany = () => {
   return (
     <div>
       <h3 style={{ color: "blue" }}>Poziom średnio zaawansowany</h3>
+      <Zadnie/>
     </div>
   );
 };
--- prototyp-broszura/src/SiteDesign/Broszura/Zaawansowany/Zaawansowany.js
+++ prototyp-broszura/src/SiteDesign/Broszura/Zaawansowany/Zaawansowany.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Zadnie } from "./Zadanie";
 
 export const Zaawansowany = () => {
   return (
     <div>
       <h3 style={{ color: "red" }}>Poziom zaawansowany</h3>
+    <Zadnie/>
     </div>
   );
 };
