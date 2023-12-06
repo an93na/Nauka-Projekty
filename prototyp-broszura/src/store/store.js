4744a9039f02acc56128126e4ef6867ba228b5cb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-06 20:21:28 +0100

    changes

--- prototyp-broszura/src/store/store.js
+++ prototyp-broszura/src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "../SiteDesign/Broszura/counterSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    punkty: counterSlice,
+  },
 });
