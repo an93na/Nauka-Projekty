3cf074349a2512bb8654575546de42a09808ffab
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-06 20:14:16 +0100

    changes

--- prototyp-broszura/src/App.js
+++ prototyp-broszura/src/App.js
@@ -2,10 +2,12 @@ import { Navigation } from "./Navigation/Navigation";
 import { TopMenu } from "./topSideDesign/TopMenu";
 
 function App() {
-  return <div className="container">
-  <TopMenu/>
-  <Navigation />
-  </div>;
+  return (
+    <div className="container">
+      <TopMenu />
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
