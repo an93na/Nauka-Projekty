279606908a37d4cae74db89533fdd27728383b9b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-06 20:18:23 +0100

    changes

---
+++ prototyp-broszura/src/SiteDesign/Broszura/counterSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "punkty",
+  initialState: {
+    value: 0,
+  },
+  reducers: {},
+});
+
+export default counterSlice.reducer
