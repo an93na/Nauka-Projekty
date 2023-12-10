e36802f8972a435e2265b5a333e2b371966b6255
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-10 20:15:34 +0100

    changes

---
+++ prototyp-broszura/src/SiteDesign/Books/counterSliceBook.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState: {
+        value: 0
+    },
+    reducers:{
+
+    }
+})
+export default bookSlice.reducer
--- prototyp-broszura/src/store/store.js
+++ prototyp-broszura/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../SiteDesign/Broszura/counterSlice";
+import bookReducer from '../SiteDesign/Books/counterSliceBook'
 
 export default configureStore({
   reducer: {
     punkty: counterSlice,
+    book: bookReducer
   },
 });
