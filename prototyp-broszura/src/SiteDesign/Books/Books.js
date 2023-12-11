df7c624be3c488e76c973f059c65a97bfa6d4807
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-11 17:33:11 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Books = () => {
+  const value = useSelector((state) => state.book.value)
   return (
     <article>
       <h4>Books</h4>
       <button>+5</button>
-      <input type="text" readOnly />
+      <input type="text" readOnly value={value}/>
       <button>-5</button>
     </article>
   );
