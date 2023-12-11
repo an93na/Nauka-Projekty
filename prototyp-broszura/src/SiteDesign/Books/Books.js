b62491ee02c57ea6ed11a643d2fbf8e86387212d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-11 17:30:39 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -4,6 +4,9 @@ export const Books = () => {
   return (
     <article>
       <h4>Books</h4>
+      <button>+5</button>
+      <input type="text" readOnly />
+      <button>-5</button>
     </article>
   );
 };
