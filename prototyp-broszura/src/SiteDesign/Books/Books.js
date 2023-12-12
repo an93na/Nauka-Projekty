c571a3226e72aa0b37e39a15673e1666f43a7126
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-12 13:39:47 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "./counterSliceBook";
 
 export const Books = () => {
+  const [pogoda, setPogoda] = useState([])
   const value = useSelector((state) => state.book.value);
   const dispatch = useDispatch();
   const api = "ac6428ea7fc0ef9caef037d08a02ce91";
   const coord = {
-    lat: 54.49, 
+    lat: 54.49,
     lon: 18.56,
   };
-  const openWeatherMapLink = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${api}`
-  console.log(openWeatherMapLink)
+  const openWeatherMapLink = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${api}`;
+  useEffect(()=>{
+    fetch(openWeatherMapLink).then((response) => response.json()).then((data) => setPogoda(data))
+  },[openWeatherMapLink])
+  
+  console.log(pogoda);
   return (
     <article>
       <h4>Books</h4>
