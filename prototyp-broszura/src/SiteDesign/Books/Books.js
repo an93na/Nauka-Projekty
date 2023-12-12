f2dc5b986d7e514d72943f786d116fdad6d56390
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-12 13:32:50 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -5,16 +5,20 @@ import { decrement, increment } from "./counterSliceBook";
 export const Books = () => {
   const value = useSelector((state) => state.book.value);
   const dispatch = useDispatch();
-  const api = 'ac6428ea7fc0ef9caef037d08a02ce91'
-  const lat = ''
-  const lon = ''
+  const api = "ac6428ea7fc0ef9caef037d08a02ce91";
+  const coord = {
+    lat: 54.49, 
+    lon: 18.56,
+  };
+  const openWeatherMapLink = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${api}`
+  console.log(openWeatherMapLink)
   return (
     <article>
       <h4>Books</h4>
       <div>
-      <button onClick={() => dispatch(increment(5))}>+5</button>
-      <input type="text" readOnly value={value} />
-      <button onClick={() => dispatch(decrement(5))}>-5</button>
+        <button onClick={() => dispatch(increment(5))}>+5</button>
+        <input type="text" readOnly value={value} />
+        <button onClick={() => dispatch(decrement(5))}>-5</button>
       </div>
     </article>
   );
