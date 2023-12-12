0e31397484b079dd3366ad59648aa68c14fa956a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-12 13:16:44 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -5,12 +5,17 @@ import { decrement, increment } from "./counterSliceBook";
 export const Books = () => {
   const value = useSelector((state) => state.book.value);
   const dispatch = useDispatch();
+  const api = 'ac6428ea7fc0ef9caef037d08a02ce91'
+  const lat = ''
+  const lon = ''
   return (
     <article>
       <h4>Books</h4>
+      <div>
       <button onClick={() => dispatch(increment(5))}>+5</button>
       <input type="text" readOnly value={value} />
       <button onClick={() => dispatch(decrement(5))}>-5</button>
+      </div>
     </article>
   );
 };
