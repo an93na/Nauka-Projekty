88c0164bdb9f9d7b26f406152180f27c66827f76
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-13 12:07:22 +0100

    changes

--- prototyp-broszura/src/SiteDesign/Books/Books.js
+++ prototyp-broszura/src/SiteDesign/Books/Books.js
@@ -5,22 +5,26 @@ import axios from "axios";
 
 export const Books = () => {
   const [city, setCity] = useState("Warszawa");
-  const [pogoda, setPogoda] = useState({});
+  const [pogoda, setPogoda] = useState({
+    name: "",
+    main: { temp: "" },
+    weather: [{ descrition: "" }],
+  });
   const value = useSelector((state) => state.book.value);
   const dispatch = useDispatch();
   const api = "ac6428ea7fc0ef9caef037d08a02ce91";
-  const coord = {
-    lat: 54.49,
-    lon: 18.56,
-  };
-  const openWeatherMapLinkGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${api}`;
+  // const coord = {
+  //   lat: 54.49,
+  //   lon: 18.56,
+  // };
+  // const openWeatherMapLinkGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${api}`;
   const openweatherMapLinkCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&lang=pl`;
 
   const fetchData = async () => {
     try {
       const response = await axios.get(openweatherMapLinkCity);
       setPogoda(response.data);
-      console.log(response.data); 
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -29,7 +33,7 @@ export const Books = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(pogoda.name)
+  console.log(pogoda.name);
   return (
     <article>
       <h4>Books</h4>
@@ -41,12 +45,14 @@ export const Books = () => {
       <div>
         <h2>Pogoda bieżąca</h2>
         <ul>
-          <li>Miasto: <span id="city">{pogoda.name}</span></li>
           <li>
-            Temperatura: <span id="temp"></span>
+            Miasto: <span id="city">{pogoda.name}</span>
           </li>
           <li>
-            Opis pogody: <span id="desc"></span>
+            Temperatura: <span id="temp">{pogoda.main.temp}°C</span>
+          </li>
+          <li>
+            Opis pogody: <span id="desc">{pogoda.weather[0].description}</span>
           </li>
           <li>
             Ikona: <img src="" id="icon" alt="icona" />
