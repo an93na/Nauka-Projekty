8f4df5aa4f6fd365414c04d86d835e09e2143292
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-27 13:53:31 +0100

    changes

--- expense-chart/src/components/Srodek.js
+++ expense-chart/src/components/Srodek.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Srodek = () => {
   return (
-    <div>Srodek</div>
+    <div style={{display: 'flex', backgroundColor: "white",
+    marginTop: '10px',      
+    padding: "20px",
+          borderRadius: "10px",
+          width: "350px",
+          alignContent: 'center',
+          justifyContent: 'space-between'
+    }}>
+        <p style={{fontWeight: 'bold', fontFamily: 'monospace'}}>Spending - Last 7 days</p>
+    </div>
   )
 }
