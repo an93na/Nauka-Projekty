95c89590d498a174d37a50005b95a5da3a14cf04
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-28 18:12:53 +0100

    changes

--- expense-chart/src/components/Srodek.js
+++ expense-chart/src/components/Srodek.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Wykresy } from "./Wykresy";
 
 export const Srodek = () => {
   return (
-    <div style={{display: 'flex', backgroundColor: "white",
-    marginTop: '10px',      
-    padding: "20px",
-          borderRadius: "10px",
-          width: "350px",
-          alignContent: 'center',
-          justifyContent: 'space-between'
-    }}>
-        <p style={{fontWeight: 'bold', fontFamily: 'monospace'}}>Spending - Last 7 days</p>
+    <div
+      style={{
+        backgroundColor: "white",
+        marginTop: "10px",
+        padding: "20px",
+        borderRadius: "10px",
+        width: "350px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <p style={{ fontWeight: "bold", fontFamily: "monospace" }}>
+          Spending - Last 7 days
+        </p>
+      </div>
+      <Wykresy />
     </div>
-  )
-}
+  );
+};
