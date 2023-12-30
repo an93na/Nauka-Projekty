f9c3f43d4fecf42516792da5f6a82a09f76a772d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-12-30 13:27:22 +0100

    changes

--- expense-chart/src/components/Podsumowanie.js
+++ expense-chart/src/components/Podsumowanie.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export const Podsumowanie = () => {
+export const Podsumowanie = (props) => {
+  const { suma } = props;
   return (
-    <div style={{ borderTopStyle: "groove", marginTop: 10, padding: 10 }}></div>
-  )
-}
+    <div style={{ borderTopStyle: "groove", marginTop: 10, padding: 10 }}>
+      <p>Total this month</p>
+      <div>
+        <h3>${suma}</h3>
+        <div>
+          <h6>+%</h6>
+          <p>from last month</p>
+        </div>
+      </div>
+    </div>
+  );
+};
