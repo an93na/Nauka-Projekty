b7359885c4aba3d2df2dab9752a9968fbcc8b304
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-17 18:18:29 +0100

    changes

---
+++ tip-calculator-app/src/komponenty/Animacja1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {classes} from './Animacja1.module.css'
+
+export const Animacja1 = () => {
+  return (
+    <div className={classes.container}>
+        <div className={classes.box}></div>
+    </div>
+  )
+}
