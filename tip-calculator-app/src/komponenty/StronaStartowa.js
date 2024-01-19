077f773080734891a4a6c4c9fbabee2ddfeb9c0d
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-19 22:00:36 +0100

    changes

--- tip-calculator-app/src/komponenty/StronaStartowa.js
+++ tip-calculator-app/src/komponenty/StronaStartowa.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import classes from '../style/Animacja1.module.css'
+import rocket from '../assets/rocket.svg'
 
 export const StronaStartowa = (props) => {
   const { setStartowa, startow } = props
   return (
     <div className={classes.containerStartowa}>
-      <button className={classes.box} onClick={() => setStartowa(true)}>
+      <button className={classes.boxStartowa} onClick={() => setStartowa(true)}>
         Rozpocznij
       </button>
+      <div>
+      <img src={rocket} alt="rocket" className={classes.rocket }/>
+      </div>
     </div>
   )
 }
