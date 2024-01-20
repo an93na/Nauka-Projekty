19c4eba6384ec54134b5f7f691ea834d9630b2e0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-20 22:07:15 +0100

    changes

--- tip-calculator-app/src/komponenty/Animacja1.js
+++ tip-calculator-app/src/komponenty/Animacja1.js
@@ -7,10 +7,15 @@ export const Animacja1 = () => {
   const [zad2, setZad2] = useState(false)
   const [zad3, setZad3] = useState(false)
   return (
-    <div className={classes.container}>
-      <Animacja nazwa="Zadanie1" zad={zad1} setZad={setZad1} />
-      <Animacja nazwa="Zadanie2" zad={zad2} setZad={setZad2} />
-      <Animacja nazwa="Zadanie3" zad={zad3} setZad={setZad3} />
+    <div>
+      <div className={classes.container}>
+        <Animacja nazwa="Zadanie1" zad={zad1} setZad={setZad1} />
+        <Animacja nazwa="Zadanie2" zad={zad2} setZad={setZad2} />
+        <Animacja nazwa="Zadanie3" zad={zad3} setZad={setZad3} />
+      </div>
+      {zad1 ? <p>zad1</p> : <p></p>}
+      {zad2 ? <p>zad2</p> : <p></p>}
+      {zad3 ? <p>zad3</p> : <p></p>}
     </div>
   )
 }
