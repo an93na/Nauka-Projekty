f7a04c8bd9093c92ccf0bcf972c7271c1571c0fd
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-21 18:13:31 +0100

    changes

--- tip-calculator-app/src/komponenty/Animacja1.js
+++ tip-calculator-app/src/komponenty/Animacja1.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Animacja } from './Animacja'
 import classes from '../style/Animacja1.module.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import { Home } from './Home'
 
 export const Animacja1 = () => {
   const [zad1, setZad1] = useState(false)
@@ -8,14 +10,27 @@ export const Animacja1 = () => {
   const [zad3, setZad3] = useState(false)
   return (
     <div>
+      <nav  className={classes.container}>
+        <Link to="/">Home</Link>
+        <Link to="/zad1">Zadanie1</Link>
+        <Link to="/zad2">Zadanie2</Link>
+        <Link to="/zad3">Zadanie3</Link>
+      </nav>
       <div className={classes.container}>
-        <Animacja nazwa="Zadanie1" zad={zad1} setZad={setZad1} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="zad1" element={<Animacja nazwa="Zadanie1" />} />
+          <Route path="zad2" element={<Animacja nazwa="Zadanie2" />} />
+          <Route path="zad3" element={<Animacja nazwa="Zadanie3" />} />
+        </Routes>
+        {/* <Animacja nazwa="Zadanie1" zad={zad1} setZad={setZad1} />
         <Animacja nazwa="Zadanie2" zad={zad2} setZad={setZad2} />
-        <Animacja nazwa="Zadanie3" zad={zad3} setZad={setZad3} />
+        <Animacja nazwa="Zadanie3" zad={zad3} setZad={setZad3} /> */}
       </div>
-      {zad1 ? <p>zad1</p> : <p></p>}
+      {/* {zad1 ? <p>zad1</p> : <p></p>}
       {zad2 ? <p>zad2</p> : <p></p>}
-      {zad3 ? <p>zad3</p> : <p></p>}
+      {zad3 ? <p>zad3</p> : <p></p>} */}
     </div>
   )
 }
