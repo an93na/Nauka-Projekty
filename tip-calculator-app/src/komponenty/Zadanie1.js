a091bb854d3b0e94396c6c99927ed72ff83d37c5
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-23 17:18:30 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie1.js
+++ tip-calculator-app/src/komponenty/Zadanie1.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Zadanie1 = () => {
   return (
     <div>
-        <h3>Zadanie1</h3>
+        <h3>Opis zadania</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius beatae molestiae tempora provident expedita. Amet error rerum quam asperiores aliquam incidunt facere odio eum, ullam fuga, harum, perferendis rem eos.
         </p>
     </div>
--- tip-calculator-app/src/komponenty/Zadanie2.js
+++ tip-calculator-app/src/komponenty/Zadanie2.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Zadanie2 = () => {
     return (
-        <div><h3>Przykład</h3></div>
+        <div>
+            <h3>Przykład</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci, consequatur cumque alias esse quas facere voluptas repudiandae placeat unde quod fugit eligendi rem voluptatum. Tenetur quia quidem praesentium necessitatibus?</p>
+        </div>
     )
 }
