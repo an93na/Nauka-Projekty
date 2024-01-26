e229bc5f4721109076231e2cb896a07521b016c6
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-26 20:04:43 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie4.js
+++ tip-calculator-app/src/komponenty/Zadanie4.js
@@ -2,24 +2,29 @@ import React from 'react'
 import classes from '../style/Animacja1.module.css'
 
 export const Zadanie4 = () => {
-  return (
-    <div className={classes.containerObr} style={{flexWrap: 'wrap', gap: 20}}>
-        <div>
-            <img src="" alt="" />
-            <input type="text" style={{width: 30}} />
+    return (
+        <div >
+            <form action="" >
+                <div className={classes.containerObr} style={{ flexWrap: 'wrap', gap: 10 }}>
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5 }}>
+                        <img src="" alt="" />
+                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    </div>
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, }}>
+                        <img src="" alt="" />
+                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    </div>
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5 }}>
+                        <img src="" alt="" />
+                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    </div>
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5 }}>
+                        <img src="" alt="" />
+                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    </div>
+                </div>
+                <button>Zatwierdź</button>
+            </form>
         </div>
-        <div>
-            <img src="" alt="" />
-            <input type="text" style={{width: 30}}/>
-        </div>
-        <div>
-            <img src="" alt="" />
-            <input type="text" style={{width: 30}} />
-        </div>
-        <div>
-            <img src="" alt="" />
-            <input type="text" style={{width: 30}}/>
-        </div>
-    </div>
-  )
+    )
 }
