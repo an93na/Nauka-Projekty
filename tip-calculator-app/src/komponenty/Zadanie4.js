125295eb1fe1d5a36d9f7a6071360f0783f16629
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-26 20:25:47 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie4.js
+++ tip-calculator-app/src/komponenty/Zadanie4.js
@@ -11,20 +11,22 @@ export const Zadanie4 = () => {
             <form action="" >
                 <div className={classes.containerObr} style={{ flexWrap: 'wrap', gap: 10 }}>
                     <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-                        <img src={mapa} alt="mapa" style={{width: 80, marginBottom: 5, borderRadius: 5}}/>
+                        <img src={mapa} alt="mapa" style={{ width: 100, marginBottom: 5, borderRadius: 5 }} />
                         <input type="text" style={{ width: 40, borderRadius: 5, padding: 5, textAlign: 'center' }} placeholder='numer' />
                     </div>
-                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, }}>
-                        <img src="" alt="" />
-                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                        <img src={serce} alt="serce" style={{ width: 100, marginBottom: 5, borderRadius: 5 }} />
+                        <input type="text" style={{ width: 40, borderRadius: 5, padding: 5, textAlign: 'center' }} placeholder='numer' />
                     </div>
-                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5 }}>
-                        <img src="" alt="" />
-                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                </div>
+                <div className={classes.containerObr} style={{ flexWrap: 'wrap', gap: 10 }}>
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                        <img src={olowek} alt="olowek" style={{ width: 100, marginBottom: 5, borderRadius: 5 }} />
+                        <input type="text" style={{ width: 40, borderRadius: 5, padding: 5, textAlign: 'center' }} placeholder='numer' />
                     </div>
-                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5 }}>
-                        <img src="" alt="" />
-                        <input type="text" style={{ width: 30 }} placeholder='nr' />
+                    <div style={{ border: '1px solid white', padding: 10, borderRadius: 5, display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                        <img src={rower} alt="rower" style={{ width: 100, marginBottom: 5, borderRadius: 5 }} />
+                        <input type="text" style={{ width: 40, borderRadius: 5, padding: 5, textAlign: 'center' }} placeholder='numer' />
                     </div>
                 </div>
                 <button>Zatwierdź</button>
