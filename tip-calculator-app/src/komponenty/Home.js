623ccc2071b0da520ed040da543eafb291c100ac
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-27 19:40:39 +0100

    changes

--- tip-calculator-app/src/komponenty/Home.js
+++ tip-calculator-app/src/komponenty/Home.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import classes from '../style/Animacja1.module.css'
 
 export const Home = () => {
     return (
         <div>
-            <h3>Home</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur voluptatem laboriosam dignissimos eos. At numquam molestias, maxime recusandae ducimus dolore asperiores esse est eligendi aut cum ipsam earum eos corrupti!
-                Minima, expedita iste qui maxime animi harum omnis quam, adipisci reprehenderit magnam tempora perferendis, quae laboriosam placeat corporis at? Ex eius quos facilis reprehenderit nisi. Ea eaque numquam voluptas magnam.
-                Praesentium vero autem nobis numquam expedita. Et sint commodi, dolore illum ea architecto in repudiandae, nulla ab error obcaecati explicabo earum quod odit placeat suscipit labore qui pariatur minus dolorem!
-                Similique soluta officia vero quidem sunt temporibus suscipit aperiam veniam sint qui, hic esse quisquam eligendi atque doloremque cumque et fugit rerum. Iusto voluptates delectus in asperiores, totam perferendis. Consectetur?</p>
+            <h3>Ewolucja Technologii Transportu</h3>
+            <p className={classes.pHome}>
+                Od starożytności człowiek dążył do udoskonalenia metod transportu. Wtedy też, około 3500 lat temu, w starożytnym Mezopotamii lub na Bliskim Wschodzie, wynaleziono koło. Początkowo było to proste koło cięte z kawałka drewna, ale stało się przełomem w historii ludzkości, umożliwiając szybszy i bardziej efektywny transport towarów i ludzi.
+            </p>
+            <p className={classes.pHome}>
+                Rozwój technologiczny nie zatrzymał się na tym etapie. Wraz z postępem cywilizacji, koło zostało stale udoskonalane, zastosowanie nowych materiałów poprawiło jego wytrzymałość i trwałość. Opony pneumatyczne, wynalezione przez Johna Dunlopa w 1888 roku, znacząco poprawiły komfort jazdy i efektywność pojazdów.
+            </p>
+            <p className={classes.pHome}>
+                Następnie nastała era maszyn parowych, które znacznie zrewolucjonizowały transport. Maszyna parowa, wynaleziona przez Jamesa Watta w XVIII wieku, doprowadziła do powstania parowozów, które stały się popularnym środkiem transportu na początku XIX wieku, umożliwiając szybkie podróże na dłuższe odległości.
+            </p>
+            <p className={classes.pHome}>
+                W dalszym ciągu postęp technologiczny nie zwalniał tempa. W 1885 roku Karl Benz skonstruował pierwszy samochód napędzany silnikiem spalinowym. Ten przełomowy wynalazek przyczynił się do kolejnej rewolucji w historii transportu, otwierając drogę do masowej produkcji samochodów.
+            </p>
+            <p className={classes.pHome}>
+                Rozwój technologiczny i inżynieria nie zatrzymały się na samochodzie. Badania nad silnikami rakietowymi, począwszy od II wojny światowej, doprowadziły do wynalezienia rakiet kosmicznych. W 1957 roku Związek Radziecki wystrzelił Sputnika 1, pierwszego sztucznego satelitę, co zapoczątkowało erę kosmiczną.
+            </p>
+            <p className={classes.pHome}>
+                Dzięki rozwojowi technologicznemu, rakietoszczelność stała się możliwa, co otworzyło drzwi do badania kosmosu i eksploracji nieznanych obszarów wszechświata. Od lądowania na Księżycu po wysyłanie sond kosmicznych, teleskopów kosmicznych i załogowych misji kosmicznych na orbitę okołoziemską i dalej, badania kosmosu stały się istotnym obszarem badań naukowych.
+            </p>
+            <p><i>Tekst wygenrowany przez: ChatGPT3.5</i></p>
         </div>
     )
 }
