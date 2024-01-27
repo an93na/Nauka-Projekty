0959df4d77bb38765101d3e562816cef37ce205d
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-27 19:57:30 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie1.js
+++ tip-calculator-app/src/komponenty/Zadanie1.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import classes from '../style/Animacja1.module.css'
 
 export const Zadanie1 = () => {
   return (
     <div>
-        <h3>Opis zadania</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius beatae molestiae tempora provident expedita. Amet error rerum quam asperiores aliquam incidunt facere odio eum, ullam fuga, harum, perferendis rem eos.
-        </p>
+      <h3>Opis zadania</h3>
+      <p className={classes.pHome}>
+        Twoim zadaniem jest ułoenie elementów w kolejności hronologicznej powstania (od lewej do prawej). Czyli w wyznaczone miejsce pod grafiką wpisujesz numerek od 1-4. 1 oznacza, że dana rzecz powstała najwczesniej, a 4 najpóźniej. Po dopasowaniu elementów wciśnij przycisk Zatwierdź.
+      </p>
+      <p className={classes.pHome}>
+        Po zatwierdzeniu pojawi się seria pytań sprawdzająca czy wiesz dlaczego dany element powinien znaleźć się na wybranym przez Ciebie miejscu.
+      </p>
     </div>
   )
 }
--- tip-calculator-app/src/komponenty/Zadanie2.js
+++ tip-calculator-app/src/komponenty/Zadanie2.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import classes from '../style/Animacja1.module.css';
+import przyklad from '../assets/przykład.png'
 
 export const Zadanie2 = () => {
     return (
         <div>
             <h3>Przykład</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci, consequatur cumque alias esse quas facere voluptas repudiandae placeat unde quod fugit eligendi rem voluptatum. Tenetur quia quidem praesentium necessitatibus?</p>
+            <p>Część 1 zadania</p>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={przyklad} alt="przykład" />
+                <p>Tutaj wpisane mamy numerki i po ich zatwierdzeniu pokazuje nam się kolejna seria pytań.</p>
+            </div>
+            <p>Część 2 zadania</p>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={przyklad} alt="przykład" />
+                <p>Tutaj mamy pytania do poprzedniej części w celu sprawdzenia naszej wiedzy.</p>
+            </div>
         </div>
     )
 }
