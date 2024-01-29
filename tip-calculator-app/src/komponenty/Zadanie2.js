b13481aaece09b8b4b26568864150d7d2a1fbc71
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-29 19:42:04 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie2.js
+++ tip-calculator-app/src/komponenty/Zadanie2.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from '../style/Animacja1.module.css';
 import przyklad from '../assets/przykład.png'
+import przyklad2 from '../assets/przyklad2.png'
 
 export const Zadanie2 = () => {
     return (
@@ -13,7 +14,7 @@ export const Zadanie2 = () => {
             </div>
             <p>Część 2 zadania</p>
             <div>
-                <img src={przyklad} alt="przykład" className={classes.imgPrzyk}/>
+                <img src={przyklad2} alt="przykład" className={classes.imgPrzyk}/>
                 <p>Tutaj mamy pytania do poprzedniej części w celu sprawdzenia naszej wiedzy.</p>
             </div>
         </div>
