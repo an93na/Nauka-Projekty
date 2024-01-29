baffb3fa74fe4d0b0b06810cd2aa0f5482fb7f21
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-29 19:36:04 +0100

    changes

--- tip-calculator-app/src/komponenty/Animacja1.js
+++ tip-calculator-app/src/komponenty/Animacja1.js
@@ -10,18 +10,18 @@ export const Animacja1 = () => {
     <div>
       <nav className={classes.container}>
         <Link to="/" className={classes.box}>Home</Link>
-        <Link to="/zad1" className={classes.box}>Opis zadania</Link>
-        <Link to="/zad2" className={classes.box}>Przykład</Link>
-        <Link to="/zad3" className={classes.box}>Zadanie</Link>
+        <Link to="/opis" className={classes.box}>Opis zadania</Link>
+        <Link to="/przyklad" className={classes.box}>Przykład</Link>
+        <Link to="/zadanie" className={classes.box}>Zadanie</Link>
       </nav>
 
       <div className={classes.paragraf}>
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="zad1" element={<Zadanie1 />} />
-          <Route path="zad2" element={<Zadanie2 />} />
-          <Route path="zad3" element={<Zadanie4 />} />
+          <Route path="opis" element={<Zadanie1 />} />
+          <Route path="przyklad" element={<Zadanie2 />} />
+          <Route path="zadanie" element={<Zadanie4 />} />
         </Routes>
       </div>
     </div>
