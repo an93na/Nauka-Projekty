88b69770360e7dfd242399b9200710df0246ebb3
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-29 19:24:26 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie2.js
+++ tip-calculator-app/src/komponenty/Zadanie2.js
@@ -4,16 +4,16 @@ import przyklad from '../assets/przykład.png'
 
 export const Zadanie2 = () => {
     return (
-        <div style={{paddingLeft:23}}>
+        <div style={{paddingLeft: '23px'}}>
             <h3>Przykład</h3>
             <p>Część 1 zadania</p>
             <div>
-                <img src={przyklad} alt="przykład" style={{width: '280px'}}/>
+                <img src={przyklad} alt="przykład" className={classes.imgPrzyk}/>
                 <p>Tutaj wpisane mamy numerki i po ich zatwierdzeniu pokazuje nam się kolejna seria pytań.</p>
             </div>
             <p>Część 2 zadania</p>
             <div>
-                <img src={przyklad} alt="przykład" style={{width: '280px'}}/>
+                <img src={przyklad} alt="przykład" className={classes.imgPrzyk}/>
                 <p>Tutaj mamy pytania do poprzedniej części w celu sprawdzenia naszej wiedzy.</p>
             </div>
         </div>
