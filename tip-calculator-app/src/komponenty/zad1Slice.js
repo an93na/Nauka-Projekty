2aa81038d6231aeff4b811901c406af56a121582
Anna Giermakowska <an.giermakowska@gmail.com>
2024-01-31 20:30:09 +0100

    changes

--- tip-calculator-app/src/komponenty/zad1Slice.js
+++ tip-calculator-app/src/komponenty/zad1Slice.js
@@ -6,7 +6,9 @@ const initialState = {
     three: '',
     four: '',
     form: false,
-    submit: false
+    submit: false,
+    showTask: false,
+    next: false
 }
 
 export const zad1Slice = createSlice({
@@ -17,10 +19,12 @@ export const zad1Slice = createSlice({
         wartoscTwo: (state, action) => { state.two = action.payload },
         wartoscThree: (state, action) => { state.three = action.payload },
         wartoscFour: (state, action) => { state.four = action.payload },
-        actionStateForm: (state) => { state.form = true},
-        actionSubmittted: (state) => { state.submit = true},
+        actionStateForm: (state) => { state.form = true },
+        actionSubmittted: (state) => { state.submit = true },
+        actionShowTask: (state) => { state.showTask = true },
+        actionNextTask: (state) => { state.next = true },
         resetState: () => { return initialState },
     }
 })
-export const { wartoscOne, wartoscTwo, wartoscThree, wartoscFour, actionStateForm, actionSubmittted, resetState } = zad1Slice.actions
+export const { wartoscOne, wartoscTwo, wartoscThree, wartoscFour, actionStateForm, actionSubmittted, resetState, actionShowTask, actionNextTask } = zad1Slice.actions
 export default zad1Slice.reducer
