d32974cca86f30ffd4a39b0df9ab3522293a51fe
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-04 23:20:27 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie5.js
+++ tip-calculator-app/src/komponenty/Zadanie5.js
@@ -4,9 +4,13 @@ import classes from '../style/Animacja1.module.css'
 export const Zadanie5 = () => {
     return (
         <div>
-            <p>III. Przed Tobą ostania czeęść zadania.</p>
+            <p>III. Przed Tobą ostania część zadania.</p>
             <p>Dopasuj nazwę do elementu:</p>
             <form action="" className={classes.zad4Form} >
+            <select name="" id="">
+                <option value="" hidden>Wybierz</option>
+                <option value=""></option>
+            </select>
             </form>
         </div>
     )
