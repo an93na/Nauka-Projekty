a3b9b0186467e9c475785fb22faf4516dacf132f
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-06 16:03:12 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie5.js
+++ tip-calculator-app/src/komponenty/Zadanie5.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import classes from '../style/Animacja1.module.css'
+import zarowka from '../assets/zarowka.png'
 
 export const Zadanie5 = () => {
     return (
         <div>
             <p>III. Przed Tobą ostania część zadania.</p>
-            <p>Dopasuj nazwę do elementu:</p>
+            <p>Dopasuj naukowca do odkrycia:</p>
             <form action="" className={classes.zad5Form} >
                 <div>
                     <img src="" alt="" />
@@ -14,11 +15,15 @@ export const Zadanie5 = () => {
                         <option value=""></option>
                     </select>
                 </div>
-                <div>
-                    <img src="" alt="" />
+                <div className={classes.zad4StyleDiv}>
+                    <p>żarówka</p>
+                    <img src={zarowka} alt="zarowka" className={classes.Zad4img} />
                     <select name="" id="">
                         <option value="" hidden>Wybierz</option>
-                        <option value=""></option>
+                        <option value="1">Thomas Edison</option>
+                        <option value="2">Ignacy Łukasiewicz</option>
+                        <option value="3">Nicola Tesla</option>
+                        <option value="4">Pitagoras</option>
                     </select>
                 </div>
                 <div>
