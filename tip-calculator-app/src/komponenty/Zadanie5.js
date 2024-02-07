791bf5c1240f6026e227694413792390b8696051
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-07 14:13:08 +0100

    changes

--- tip-calculator-app/src/komponenty/Zadanie5.js
+++ tip-calculator-app/src/komponenty/Zadanie5.js
@@ -8,13 +8,6 @@ export const Zadanie5 = () => {
             <p>III. Przed Tobą ostania część zadania.</p>
             <p>Dopasuj naukowca do odkrycia:</p>
             <form action="" className={classes.zad5Form} >
-                <div>
-                    <img src="" alt="" />
-                    <select name="" id="">
-                        <option value="" hidden>Wybierz</option>
-                        <option value=""></option>
-                    </select>
-                </div>
                 <div className={classes.zad5StyleDiv}>
                     <p>żarówka</p>
                     <img src={zarowka} alt="zarowka" className={classes.Zad5img} />
@@ -26,20 +19,40 @@ export const Zadanie5 = () => {
                         <option value="4">Pitagoras</option>
                     </select>
                 </div>
-                <div>
-                    <img src="" alt="" />
+                <div className={classes.zad5StyleDiv}>
+                    <p>teoria heliocentryczna</p>
+                    <img src={zarowka} alt="zarowka" className={classes.Zad5img} />
                     <select name="" id="">
                         <option value="" hidden>Wybierz</option>
-                        <option value=""></option>
+                        <option value="1">Neil Armstrong</option>
+                        <option value="2">Mikołaj Kopernik</option>
+                        <option value="3">Galileo Galilei</option>
+                        <option value="4">Johannes Kepler</option>
                     </select>
                 </div>
-                <div>
-                    <img src="" alt="" />
+                <div className={classes.zad5StyleDiv}>
+                    <p>zjawisko grawitacji</p>
+                    <img src={zarowka} alt="zarowka" className={classes.Zad5img} />
                     <select name="" id="">
                         <option value="" hidden>Wybierz</option>
-                        <option value=""></option>
+                        <option value="1">Maria Skłodowska-Curie</option>
+                        <option value="2">Isaac Newton</option>
+                        <option value="3">Alfred Nobel</option>
+                        <option value="4">Leonardo da Vinci</option>
                     </select>
                 </div>
+                <div className={classes.zad5StyleDiv}>
+                    <p>prawo wyporu</p>
+                    <img src={zarowka} alt="zarowka" className={classes.Zad5img} />
+                    <select name="" id="">
+                        <option value="" hidden>Wybierz</option>
+                        <option value="1">Galileusz</option>
+                        <option value="2">Charles Darwin</option>
+                        <option value="3">Pitagoras</option>
+                        <option value="4">Archimedes</option>
+                    </select>
+                </div>
+
             </form>
         </div>
     )
