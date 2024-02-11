47a038b45518f9b08844d5d3f24cbe134ee4dcb0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-11 20:28:47 +0100

    changes

--- my-app/src/components/StronaStartowa.js
+++ my-app/src/components/StronaStartowa.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
-
+import obraz from '../assets/obrazTytulowa.jpg'
 export const StronaStartowa = () => {
   return (
     <div className={classes.startowa}>
+      <img src={obraz} alt="strona tytułowa" className={classes.imgStart} />
         <button className={classes.btnStart}>Rozpocznij</button>
     </div>
   )
