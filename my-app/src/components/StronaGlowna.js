66384c8f3cbbd4d68cac98014e8beed4e7e8ff16
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-12 21:01:05 +0100

    changes

--- my-app/src/components/StronaGlowna.js
+++ my-app/src/components/StronaGlowna.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const StronaGlowna = () => {
   return (
     <div>
       <nav>
-        
+        <Link to='/'>Home</Link>
       </nav>
     </div>
   )
