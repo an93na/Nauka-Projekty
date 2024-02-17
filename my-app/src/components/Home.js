499ee99e63f52150d387de59e590600589094dce
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-17 19:27:45 +0100

    changes

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
+import homeImg from '../assets/home.jpg';
 
 export const Home = () => {
   return (
     <div className={classes.sekcja}>
-      <p>Home</p>
+      <h3>Home</h3>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+      <div>
+      <p style={{marginTop: 0, marginBottom: 0}}>Witaj Szanowny Panie albo Pani.</p>
+      <p style={{marginTop: 0, marginBottom: 0}}> Miło, że odwiedzasz moją stronę. Nie znajdziesz tu jednak żadnych informacji o mnie ponieważ wszystkie otrzymałeś wcześniej.</p> 
+      <p style={{marginTop: 0, marginBottom: 0}}> Teraz sprawdzimy jak wnikliwie je przeanalizowałeś :D </p>
+      </div>
+      <img src={homeImg} alt="homeImg" className={classes.imgHome} />
+      </div>
     </div>
   )
 }
