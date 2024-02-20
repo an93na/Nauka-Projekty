31bc358ffe56f31f796a9b019d27a553e39979a8
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-20 17:23:32 +0100

    changes

--- my-app/src/components/Opis.js
+++ my-app/src/components/Opis.js
@@ -7,7 +7,8 @@ export const Opis = () => {
       <h3>Opis</h3>
       <div className={classes.pOpis}>
         <p>Podobno najtrudniej jest zacząć ale spokojnie myślę, że się dogadamy.</p>
-        <p>Twoim zadaniem jest odpowiedzenie na serię kilku pytań. W pierwszej części trzeba dopasować cechy w odpowiedniej kolejności wpisując 1-6. </p>
+        <p>Twoim zadaniem jest odpowiedzenie na serię kilku pytań. </p>
+        <p> W pierwszej części trzeba dopasować cechy w odpowiedniej kolejności wpisując 1-6. </p>
         <p>Druga część to test złożony z pytań jednokrotnego wyboru</p>
         <p>Natomiast trzecia część ostatnia to też pytania jednokrotnego wyboru. </p>
         <p>Nie pozostaje mi nic innego jak życzyć powodzenia! </p>
