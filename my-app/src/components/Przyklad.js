2d7ab76d59524df754daec319d89bab07cdc3cb1
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-23 18:28:10 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import classes from '../style/StyleModule.module.css'
 
 export const Przyklad = () => {
   return (
-    <div>Przyklad</div>
+    <div className={classes.sekcja}>
+      <h3>
+        Przyklad
+      </h3>
+    </div>
   )
 }
