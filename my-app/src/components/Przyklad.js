44d5e9a587dc25dd879843d451912f1fddeb5aea
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-23 18:36:04 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -9,7 +9,8 @@ export const Przyklad = () => {
         Przyklad
       </h3>
       <p>Część 1 zadania</p>
-      <img src="" alt="" />
+      <img src={przyklad1} alt="przyklad1" className={classes.imgOpis} />
+      <p>Po wpisaniu numerków klikamy przycisk Zatwierdź i jeśli jest dobrze do przechodzimy do drugiego zadania</p>
     </div>
   )
 }
