82316273495ca7f6e0fa220ec2c769425f7d705a
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-24 19:12:10 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -10,10 +10,10 @@ export const Przyklad = () => {
         Przyklad
       </h3>
       <p>Część 1 zadania</p>
-      <img src={przyklad1} alt="przyklad1" className={classes.imgOpis} />
+      <img src={przyklad1} alt="przyklad1" className={classes.imgPrzyklad} />
       <p>Po wpisaniu numerków klikamy przycisk Zatwierdź i jeśli jest dobrze do przechodzimy do drugiego zadania</p>
       <p>Część 2 zadania</p>
-      <img src={przyklad2} alt="przyklad1" className={classes.imgOpis} />
+      <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
       <p>W tej części wybieramy jedną prawidłową odpowiedź. A następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to przechodzimy do ostatniego zadania</p>
     </div>
   )
