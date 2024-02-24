a4ca4d90e5fd206f357a8b592e4ea3fe72433de2
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-24 19:08:00 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
 import przyklad1 from '../assets/przyklad1.png'
+import przyklad2 from '../assets/przyklad2.png'
 
 export const Przyklad = () => {
   return (
@@ -11,6 +12,9 @@ export const Przyklad = () => {
       <p>Część 1 zadania</p>
       <img src={przyklad1} alt="przyklad1" className={classes.imgOpis} />
       <p>Po wpisaniu numerków klikamy przycisk Zatwierdź i jeśli jest dobrze do przechodzimy do drugiego zadania</p>
+      <p>Część 2 zadania</p>
+      <img src={przyklad2} alt="przyklad1" className={classes.imgOpis} />
+      <p>W tej części wybieramy jedną prawidłową odpowiedź. A następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to przechodzimy do ostatniego zadania</p>
     </div>
   )
 }
