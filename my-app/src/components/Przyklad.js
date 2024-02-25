f2e1d4bc623bb73e8c056a4c601a8cce1f3f1fe6
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-25 18:06:15 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -19,6 +19,12 @@ export const Przyklad = () => {
       <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
       </div>
       <p>W tej części wybieramy jedną prawidłową odpowiedź. A następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to przechodzimy do ostatniego zadania</p>
+      <p>Część 3 zadania</p>
+      <div className={classes.divPrzyklad}>
+      <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
+      </div>
+      <p>W tej części wybieramy jedną prawidłową odpowiedź z rozwijanej listy. Następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to udało Ci się ukończyć całe zadanie.</p>
+      <p>Jeśli gdzieś się pomylisz lub chcesz spróbować jeszcze raz to kliknij przycisk Reset albo wyczyść, po sekcji ktorą chcesz poprawić</p>
     </div>
   )
 }
