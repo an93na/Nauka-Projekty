70813a3e5c1afb841e0dd16975955e88653a4c16
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-25 18:12:30 +0100

    changes

--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from '../style/StyleModule.module.css'
 import przyklad1 from '../assets/przyklad1.png'
 import przyklad2 from '../assets/przyklad2.png'
+import przyklad3 from '../assets/przyklad3.png'
 
 export const Przyklad = () => {
   return (
@@ -21,7 +22,7 @@ export const Przyklad = () => {
       <p>W tej części wybieramy jedną prawidłową odpowiedź. A następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to przechodzimy do ostatniego zadania</p>
       <p>Część 3 zadania</p>
       <div className={classes.divPrzyklad}>
-      <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
+      <img src={przyklad3} alt="przyklad1" className={classes.imgPrzyklad3} />
       </div>
       <p>W tej części wybieramy jedną prawidłową odpowiedź z rozwijanej listy. Następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to udało Ci się ukończyć całe zadanie.</p>
       <p>Jeśli gdzieś się pomylisz lub chcesz spróbować jeszcze raz to kliknij przycisk Reset albo wyczyść, po sekcji ktorą chcesz poprawić</p>
