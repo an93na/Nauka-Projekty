a813b036b123b36d6e7ed58b82a60b91e0a860e0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-27 18:55:34 +0100

    changes

--- my-app/src/components/Zadanie.js
+++ my-app/src/components/Zadanie.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
+import { Zadanie1 } from './Zadanie1'
 
 export const Zadanie = () => {
   return (
     <div className={classes.sekcja}>
       <h3>Zadanie</h3>
-      <p>Część I</p>
-      <p>W CV w umiejętnościach miękkich wymieniłam trzy najważniejsze cechy jakie posiadam. Twoim zadaniem jest uporządkować je w tej samej kolejności. Słusznie zauważyłeś, że tam były trzy cechy, a tu jest cztery. Ta czwarta to bonus i powinna się znaleźć na końcu.</p>
-
+      <Zadanie1 />
     </div>
   )
 }
---
+++ my-app/src/components/Zadanie1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Zadanie1 = () => {
+    return (
+        <div>
+            <p>Część I</p>
+            <p>W CV w umiejętnościach miękkich wymieniłam trzy najważniejsze cechy jakie posiadam. Twoim zadaniem jest uporządkować je w tej samej kolejności. Słusznie zauważyłeś, że tam były trzy cechy, a tu jest cztery. Ta czwarta to bonus i powinna się znaleźć na końcu.</p>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <input type="text" />
+            </div>
+        </div>
+        </div>
+    )
+}
