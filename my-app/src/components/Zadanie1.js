1a96d89b82be54265fd7ad11d221195aaf2fb1f9
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-28 19:28:45 +0100

    changes

--- my-app/src/components/Zadanie1.js
+++ my-app/src/components/Zadanie1.js
@@ -10,25 +10,25 @@ export const Zadanie1 = () => {
         <div>
             <p>Część I</p>
             <p>W CV w umiejętnościach miękkich wymieniłam trzy najważniejsze cechy jakie posiadam. Twoim zadaniem jest uporządkować je w tej samej kolejności. Słusznie zauważyłeś, że tam były trzy cechy, a tu jest cztery. Ta czwarta to bonus i powinna się znaleźć na końcu.</p>
-            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            <div style={{display: 'flex', flexWrap: 'wrap', alignContent: 'center', justifyContent: 'center', alignItems: 'center'}}>
                 <div className={classes.zadDiv}>
-                    <h5>Szybkie podejmowanie decyzji</h5>
+                    <h5>Praca zespołowa</h5>
                     <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
                     <input type="text" className={classes.pZad1}/>
                 </div>
                 <div className={classes.zadDiv}>
                     <h5>Szybkie podejmowanie decyzji</h5>
-                    <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                    <img src={z1b} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
                     <input type="text" className={classes.pZad1} />
                 </div>
                 <div className={classes.zadDiv}>
-                    <h5>Szybkie podejmowanie decyzji</h5>
-                    <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                    <h5>szybkie podejmowanie decyzji</h5>
+                    <img src={z1c} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
                     <input type="text" className={classes.pZad1}/>
                 </div>
                 <div className={classes.zadDiv}>
-                    <h5>Szybkie podejmowanie decyzji</h5>
-                    <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                    <h5>kreatywność i analityczne myślenie</h5>
+                    <img src={z1d} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
                     <input type="text" className={classes.pZad1}/>
                 </div>
             </div>
