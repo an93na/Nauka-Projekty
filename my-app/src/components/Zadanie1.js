f5c94662002df8fddbbd1d51a545620d88370842
Anna Giermakowska <an.giermakowska@gmail.com>
2024-02-29 19:28:00 +0100

    changes

--- my-app/src/components/Zadanie1.js
+++ my-app/src/components/Zadanie1.js
@@ -10,28 +10,31 @@ export const Zadanie1 = () => {
         <div>
             <p>Część I</p>
             <p>W CV w umiejętnościach miękkich wymieniłam trzy najważniejsze cechy jakie posiadam. Twoim zadaniem jest uporządkować je w tej samej kolejności. Słusznie zauważyłeś, że tam były trzy cechy, a tu jest cztery. Ta czwarta to bonus i powinna się znaleźć na końcu.</p>
-            <div style={{display: 'flex', flexWrap: 'wrap', alignContent: 'center', justifyContent: 'center', alignItems: 'center'}}>
-                <div className={classes.zadDiv} style={{ height: '40vh' }}>
-                    <h5>praca zespołowa</h5>
-                    <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
-                    <input type="text" className={classes.pZad1}/>
+            <form action="" style={{textAlign: 'center'}}>
+                <div style={{ display: 'flex', flexWrap: 'wrap', alignContent: 'center', justifyContent: 'center', alignItems: 'center' }}>
+                    <div className={classes.zadDiv} style={{ height: '40vh' }}>
+                        <h5>praca zespołowa</h5>
+                        <img src={z1a} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                        <input type="text" className={classes.pZad1} />
+                    </div>
+                    <div className={classes.zadDiv} style={{ height: '40vh' }}>
+                        <h5>komunikatywność i zaangażowanie</h5>
+                        <img src={z1b} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                        <input type="text" className={classes.pZad1} />
+                    </div>
+                    <div className={classes.zadDiv} style={{ height: '40vh' }}>
+                        <h5>szybkie podejmowanie decyzji</h5>
+                        <img src={z1c} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                        <input type="text" className={classes.pZad1} />
+                    </div>
+                    <div className={classes.zadDiv} style={{ height: '40vh' }}>
+                        <h5>kreatywność i analityczne myślenie</h5>
+                        <img src={z1d} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
+                        <input type="text" className={classes.pZad1} />
+                    </div>
                 </div>
-                <div className={classes.zadDiv} style={{ height: '40vh' }}>
-                    <h5>komunikatywność i zaangażowanie</h5>
-                    <img src={z1b} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
-                    <input type="text" className={classes.pZad1} />
-                </div>
-                <div className={classes.zadDiv} style={{ height: '40vh' }}>
-                    <h5>szybkie podejmowanie decyzji</h5>
-                    <img src={z1c} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
-                    <input type="text" className={classes.pZad1}/>
-                </div>
-                <div className={classes.zadDiv} style={{ height: '40vh' }}>
-                    <h5>kreatywność i analityczne myślenie</h5>
-                    <img src={z1d} alt="szybkie podejmowanie decyzji" className={classes.imgZad1} />
-                    <input type="text" className={classes.pZad1}/>
-                </div>
-            </div>
+                <button type='submit'className={classes.btnZ1}>Zatwierdź</button>
+            </form>
         </div>
     )
 }
