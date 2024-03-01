426fdd167be6cfd42c8e9f84365958bca7fd2ee4
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-01 19:49:39 +0100

    changes

--- my-app/src/components/Zadanie.js
+++ my-app/src/components/Zadanie.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
 import { Zadanie1 } from './Zadanie1'
+import { Zadanie2 } from './Zadanie2'
 
 export const Zadanie = () => {
   return (
     <div className={classes.sekcja}>
       <h3>Zadanie</h3>
       <Zadanie1 />
+      <Zadanie2/>
     </div>
   )
 }
---
+++ my-app/src/components/Zadanie2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Zadanie2 = () => {
+  return (
+    <div>
+        <p>Część II</p>
+        <p>W tym zadaniu należy wybrać jedną prawidłową odpowiedź. Nie ma łatwo ponieważ tutaj zakresem wiedzy jest całe moje CV.</p>
+        <form action="">
+            
+        </form>
+    </div>
+  )
+}
