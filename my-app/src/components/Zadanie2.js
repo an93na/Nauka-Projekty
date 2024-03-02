a8e042cb5a08b3d9b144749ded687d92f014b4c4
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-02 19:56:37 +0100

    changes

--- my-app/src/components/Zadanie2.js
+++ my-app/src/components/Zadanie2.js
@@ -6,7 +6,7 @@ export const Zadanie2 = () => {
         <div>
             <p>Część II</p>
             <p>W tym zadaniu należy wybrać jedną prawidłową odpowiedź. Nie ma łatwo ponieważ tutaj zakresem wiedzy jest całe moje CV.</p>
-            <form action="">
+            <form action="" style={{display: 'flex', flexDirection: 'column'}}>
                 <p>1) Ile umiejętności technicznych wymieniłam?</p>
                 <label>
                     <input type='radio' /> 1
