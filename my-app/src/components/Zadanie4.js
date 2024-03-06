b3e7e9a399087f1f24653257793eacc9c4fa1327
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-06 19:33:40 +0100

    changes

--- my-app/src/components/Zadanie4.js
+++ my-app/src/components/Zadanie4.js
@@ -6,15 +6,33 @@ export const Zadanie4 = () => {
     <div>
         <p>Krzyżówka</p>
         <form action="">
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <input type="text" className={classes.krzyz} />
+            <div className={classes.krzyz2}></div>
+            </div>
             <div style={{display: 'flex'}}>
+              <p>2.</p>
+            <div className={classes.krzyz2}></div>
+            <div className={classes.krzyz2}></div>
+            <div className={classes.krzyz2}></div>
+            <div className={classes.krzyz2}></div>
             <input type="text" className={classes.krzyz} />
             <input type="text" className={classes.krzyz} />
             <input type="text" className={classes.krzyz} />
             <input type="text" className={classes.krzyz} />
             <input type="text" className={classes.krzyz} />
             <input type="text" className={classes.krzyz} />
-            <input type="text" className={classes.krzyz} />
-
+            <div className={classes.krzyz}></div>
             </div>
         </form>
     </div>
