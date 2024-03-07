147e76b6fe184747b92f3a174dec4fedbc128d0c
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-07 19:45:12 +0100

    changes

--- my-app/src/components/Zadanie4.js
+++ my-app/src/components/Zadanie4.js
@@ -3,127 +3,32 @@ import classes from '../style/StyleModule.module.css'
 
 export const Zadanie4 = () => {
   return (
-    <div>
+    <div style={{marginRight: '5px'}}>
       <p>Krzyżówka</p>
       <form action="">
         <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%'}}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '50%'}}>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-            <input type="text" className={classes.krzyz}/>
-          </div>
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+          <div className={classes.krzyz} ></div>
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
+          <input type="text" className={classes.krzyz} />
         </div>
       </form>
     </div>
