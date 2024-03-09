7e35e14105a47020b28b7e9a8e9c4bc7682b0d9a
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-09 20:04:45 +0100

    changes

--- my-app/src/slice/Zad1slice.js
+++ my-app/src/slice/Zad1slice.js
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
-
+    one: '',
+    two: '',
+    three: '',
+    four: '',
+    submit: false,
 }
 
 export const Zad1Slice = createSlice({
     name: 'zad1',
     initialState: initialState,
-    reducers: {}
+    reducers: {
+        odpOne: (state, action) => {state.one = action.payload},
+        odpTwo: (state, action) => {state.two = action.payload},
+        odpThree: (state, action) => {state.three = action.payload},
+        odpFour: (state, action) => {state.four = action.payload}
+    }
 })
 
 export default Zad1Slice.reducer
