12ea7ee91eba2b15cea9266eff767a31911415b6
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-18 16:33:12 +0100

    changes

--- my-app/src/slice/Zad2slice.js
+++ my-app/src/slice/Zad2slice.js
@@ -1,2 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const zad2Slice = createSlice({
+    name: 'zadanie2',
+    initialState: {},
+    reducers: {}
+})
+
+export default zad2Slice.reducer
