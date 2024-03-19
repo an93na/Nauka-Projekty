67cbd63d5d6f1ab586058f7ae65dab2cde562d4f
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-19 17:13:30 +0100

    changes

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import zad1Reducer from '../slice/Zad1slice'
+import zad2Reducer from "../slice/Zad2slice"
 
 export const store = configureStore({
     reducer: {
-        zad1: zad1Reducer
+        zad1: zad1Reducer,
+        zad2: zad2Reducer
     }
 })
