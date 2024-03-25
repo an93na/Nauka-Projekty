b043d46316e0730a60cc4934189bb6d9a41f9a28
Anna Giermakowska <an.giermakowska@gmail.com>
2024-03-25 19:33:21 +0100

    changes

--- my-app/src/components/Zadanie2.js
+++ my-app/src/components/Zadanie2.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { four, one, three, two } from '../slice/Zad2slice';
+import { five, four, one, three, two } from '../slice/Zad2slice';
 
 export const Zadanie2 = () => {
     const oneValue = useSelector(state => state.zad2.one);
@@ -57,29 +57,29 @@ export const Zadanie2 = () => {
                     </label>
                     <p>4) W którym roku uczestniczyłam w kursie Front-end developer?</p>
                     <label>
-                        <input type='radio' name='4' value={'1'} onChange={(e) => dispatch(four(e.target.value))}/> 2020/2021
+                        <input type='radio' name='4' value={'1'} onChange={(e) => dispatch(four(e.target.value))} /> 2020/2021
                     </label>
                     <label>
-                        <input type='radio' name='4' value={'2'} onChange={(e) => dispatch(four(e.target.value))}/> 2021/2022
+                        <input type='radio' name='4' value={'2'} onChange={(e) => dispatch(four(e.target.value))} /> 2021/2022
                     </label>
                     <label>
-                        <input type='radio' name='4' value={'3'} onChange={(e) => dispatch(four(e.target.value))}/> 2022/2023
+                        <input type='radio' name='4' value={'3'} onChange={(e) => dispatch(four(e.target.value))} /> 2022/2023
                     </label>
                     <label>
-                        <input type='radio' name='4' value={'4'} onChange={(e) => dispatch(four(e.target.value))}/> 2023/2024
+                        <input type='radio' name='4' value={'4'} onChange={(e) => dispatch(four(e.target.value))} /> 2023/2024
                     </label>
                     <p>5) Jakim jezyku obcy oprócz angielskiego wspominam w CV?</p>
                     <label>
-                        <input type='radio' name='5' value={'1'} /> niemiecki
+                        <input type='radio' name='5' value={'1'} onChange={(e) => dispatch(five(e.target.value))} /> niemiecki
                     </label>
                     <label>
-                        <input type='radio' name='5' value={'2'} /> rosyjski
+                        <input type='radio' name='5' value={'2'} onChange={(e) => dispatch(five(e.target.value))} /> rosyjski
                     </label>
                     <label>
-                        <input type='radio' name='5' value={'3'} /> francuski
+                        <input type='radio' name='5' value={'3'} onChange={(e) => dispatch(five(e.target.value))} /> francuski
                     </label>
                     <label>
-                        <input type='radio' name='5' value={'4'} /> hiszpański
+                        <input type='radio' name='5' value={'4'} onChange={(e) => dispatch(five(e.target.value))} /> hiszpański
                     </label>
                 </div>
                 <button type='submit' className={classes.btnZ1}>Zatwierdź</button>
