d6cf472662f21916744dd60262b5e73953ce2713
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-01 15:47:43 +0200

    changes

--- my-app/src/slice/Zad2slice.js
+++ my-app/src/slice/Zad2slice.js
@@ -1,28 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    one: '',
-    two: '',
-    three: '',
-    four: '',
-    five: '',
     submit: false,
     submitted: false,
-    nextTask: false
+    nextTask: false,
+    showNextTask: false,
 }
 
 export const zad2Slice = createSlice({
     name: 'zadanie2',
     initialState: initialState,
     reducers: {
-        one: (state, action) => { state.one = action.payload },
-        two: (state, action) => { state.two = action.payload },
-        three: (state, action) => { state.three = action.payload },
-        four: (state, action) => { state.four = action.payload },
-        five: (state, action) => { state.five = action.payload },
         submit: (state) => { state.submit = true },
         actionSubmitted: (state) => { state.submitted = true },
         nextTask: (state) => { state.nextTask = true },
+        showNextTask: (state) => { state.showNextTask = true },
         resetState: () => { return initialState }
     }
 })
