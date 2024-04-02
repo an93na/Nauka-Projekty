d4b5e2d96326ac6f2c987d8b3f266c01d0a914bf
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-02 15:39:14 +0200

    changes

---
+++ my-app/src/slice/Zad3slice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const Zad3Slice = createSlice({
+    name: 'zadanie3',
+    initialState: {},
+    reducers: {}
+})
+
+export default Zad3Slice.reducer
--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import zad1Reducer from '../slice/Zad1slice'
 import zad2Reducer from "../slice/Zad2slice"
+import zad3Reducer from "../slice/Zad3slice"
 
 export const store = configureStore({
     reducer: {
         zad1: zad1Reducer,
-        zad2: zad2Reducer
+        zad2: zad2Reducer,
+        zad3: zad3Reducer
     }
 })
