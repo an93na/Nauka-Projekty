0cfcb898b70e051100a348522d983804472777fb
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-02 15:42:09 +0200

    changes

--- my-app/src/slice/Zad3slice.js
+++ my-app/src/slice/Zad3slice.js
@@ -1,8 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+const initialState = {
+    one: '',
+    two: '',
+    three: '',
+    four: '',
+    submit: false,
+    right: false
+}
+
 export const Zad3Slice = createSlice({
     name: 'zadanie3',
-    initialState: {},
+    initialState: initialState,
     reducers: {}
 })
 
