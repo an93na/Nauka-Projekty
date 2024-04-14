3dff8d6323a2bbb6a6a9f9e26dde2f5490186cf0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-14 20:12:59 +0200

    changes

--- my-app/src/components/Zadanie.js
+++ my-app/src/components/Zadanie.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import classes from '../style/StyleModule.module.css'
 import { Zadanie1 } from './Zadanie1'
-import { Zadanie2 } from './Zadanie2'
-import { Zadanie3 } from './Zadanie3'
-import { Zadanie4 } from './Zadanie4'
 
 export const Zadanie = () => {
   return (
     <div className={classes.sekcja}>
       <h3>Zadanie</h3>
       <Zadanie1 />
-      {/* <Zadanie2 /> */}
-      {/* <Zadanie3 /> */}
     </div>
   )
 }
