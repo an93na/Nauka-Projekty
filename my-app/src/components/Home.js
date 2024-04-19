c588f05fb2111fda526e2e162b97d337dc4cefae
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-19 20:18:21 +0200

    changes

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -6,13 +6,13 @@ export const Home = () => {
   return (
     <div className={classes.sekcja}>
       <h3>Home</h3>
-      <div style={{display: 'flex', alignItems: 'center'}}>
-      <div>
-      <p className={classes.pHome}>Witaj Szanowny Panie albo Pani.</p>
-      <p className={classes.pHome}> Miło, że odwiedzasz moją stronę. Nie znajdziesz tu jednak żadnych informacji o mnie ponieważ wszystkie otrzymałeś wcześniej.</p> 
-      <p className={classes.pHome}> Teraz sprawdzimy jak wnikliwie je przeanalizowałeś :D </p>
-      </div>
-      <img src={homeImg} alt="homeImg" className={classes.imgHome} />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>
+          <p className={classes.pHome}>Witaj Szanowny Panie albo Pani.</p>
+          <p className={classes.pHome}> Miło, że odwiedzasz moją stronę. Nie znajdziesz tu jednak żadnych informacji o mnie ponieważ wszystkie otrzymałeś wcześniej.</p>
+          <p className={classes.pHome}> Teraz sprawdzimy jak wnikliwie je przeanalizowałeś :D </p>
+        </div>
+        <img src={homeImg} alt="homeImg" className={classes.imgHome} />
       </div>
     </div>
   )
--- my-app/src/components/Przyklad.js
+++ my-app/src/components/Przyklad.js
@@ -13,17 +13,17 @@ export const Przyklad = () => {
       <p>Część I zadania</p>
       <p className={classes.pPrzyklad}>Jeśli gdzieś się pomylisz lub chcesz spróbować jeszcze raz to kliknij przycisk Reset albo wyczyść, po sekcji ktorą chcesz poprawić</p>
       <div className={classes.divPrzyklad}>
-      <img src={przyklad1} alt="przyklad1" className={classes.imgPrzyklad} />
+        <img src={przyklad1} alt="przyklad1" className={classes.imgPrzyklad} />
       </div>
       <p className={classes.pPrzyklad}>Po wpisaniu numerków klikamy przycisk Zatwierdź i jeśli jest dobrze do przechodzimy do drugiego zadania</p>
       <p>Część II zadania</p>
       <div className={classes.divPrzyklad}>
-      <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
+        <img src={przyklad2} alt="przyklad1" className={classes.imgPrzyklad} />
       </div>
       <p className={classes.pPrzyklad}>W tej części wybieramy jedną prawidłową odpowiedź. A następnie klikamy przycisk Zatwierdź. Jeśli odpowiedzi są prawidłowe to przechodzimy do ostatniego zadania</p>
       <p>Część III zadania</p>
       <div className={classes.divPrzyklad}>
-      <img src={przyklad3} alt="przyklad1" className={classes.imgPrzyklad3} />
+        <img src={przyklad3} alt="przyklad1" className={classes.imgPrzyklad3} />
       </div>
       <p className={classes.pPrzyklad}>W tej części wybieramy jedną prawidłową odpowiedź z rozwijanej listy. Następnie klikamy przycisk Sprawdź. Jeśli odpowiedzi są prawidłowe to udało Ci się ukończyć całe zadanie.</p>
     </div>
