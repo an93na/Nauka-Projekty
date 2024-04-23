5261c7022856b92ea400349f169478b26d041ac8
Anna Giermakowska <an.giermakowska@gmail.com>
2024-04-23 20:07:41 +0200

    changes

--- cwiczenie-app/src/components/Klasy.js
+++ cwiczenie-app/src/components/Klasy.js
@@ -10,16 +10,19 @@ export const Klasy = () => {
     }
 
     class Zwierze {
-        constructor(rodzajVal, rasaVal, wiekVal, imieVal){
+        constructor(rodzajVal, rasaVal, wiekVal, imieVal) {
             this.rodzaj = rodzajVal
-            this.rasa= rasaVal
-            this.wiek= wiekVal
-            this.imie= imieVal
+            this.rasa = rasaVal
+            this.wiek = wiekVal
+            this.imie = imieVal
         }
     }
 
     const kowalski = new Osoba('Jan', 'Kowalski')
     console.log(kowalski)
+
+    const azor = new Zwierze('pies', 'lablador', 3, 'Azor')
+    console.log(azor)
 
     return (
         <div><h3>Klasy</h3>
