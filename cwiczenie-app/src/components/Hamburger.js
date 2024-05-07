2758e80f9bccbec2f0b63642f829afae6a6cf047
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-07 18:58:48 +0200

    changes

--- cwiczenie-app/src/components/Hamburger.js
+++ cwiczenie-app/src/components/Hamburger.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import classes from '../style/Style.module.css'
 
 export const Hamburger = (props) => {
-    const {} = props
-  return (
-    <div>Hamburger</div>
-  )
+    const { setIsOpen } = props
+    return (
+        <div id='hamburger' className={classes.hamburger} onClick={() => { setIsOpen(prev => !prev) }}>
+            <div className={classes.linia}></div>
+            <div className={classes.linia}></div>
+            <div className={classes.linia}></div></div>
+    )
 }
--- cwiczenie-app/src/components/Krzyzyk.js
+++ cwiczenie-app/src/components/Krzyzyk.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from '../style/Style.module.css'
 
 export const Krzyzyk = (props) => {
-    const {} = props
-  return (
-    <div>Krzyzyk</div>
-  )
+    const { setIsOpen } = props
+    return (
+        <div className={classes.krzyzyk} onClick={() => { setIsOpen(prev => !prev) }}></div>
+    )
 }
