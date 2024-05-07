e3df116784cfba70e1d8416c8dafe5f8e6106051
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-07 18:56:16 +0200

    changes

---
+++ cwiczenie-app/src/components/Hamburger.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Hamburger = (props) => {
+    const {} = props
+  return (
+    <div>Hamburger</div>
+  )
+}
---
+++ cwiczenie-app/src/components/Krzyzyk.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Krzyzyk = (props) => {
+    const {} = props
+  return (
+    <div>Krzyzyk</div>
+  )
+}
