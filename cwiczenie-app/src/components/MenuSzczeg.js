36e6321e6c45627fb418a7ba2cc50b87dc6a726c
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-10 19:29:33 +0200

    changes

--- cwiczenie-app/src/components/MenuSzczeg.js
+++ cwiczenie-app/src/components/MenuSzczeg.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import classes from '../style/Style.module.css'
+import { Link, Route, Routes } from 'react-router-dom'
 
 export const MenuSzczeg = () => {
   return (
-    <div className={classes.menuszcze}>MenuSzczeg</div>
+    <div className={classes.menuszcze}>
+      <Link></Link>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
   )
 }
