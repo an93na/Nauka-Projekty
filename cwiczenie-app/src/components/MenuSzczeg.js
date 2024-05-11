9f2bd3d3c1e28cecadf157b4e265bf412aa25b51
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-11 17:47:48 +0200

    changes

--- cwiczenie-app/src/components/MenuSzczeg.js
+++ cwiczenie-app/src/components/MenuSzczeg.js
@@ -2,18 +2,26 @@ import React from 'react'
 import classes from '../style/Style.module.css'
 import { Link, Route, Routes } from 'react-router-dom'
 import { Klasy } from './Klasy'
+import { Home } from './Home'
+import { Page1 } from './Page1'
+import { Page2 } from './Page2'
 
 export const MenuSzczeg = () => {
   return (
-    <div className={classes.menuszcze}>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/Page1'>Page1</Link>
-        <Link to='/Page2'>Page2</Link>
+    <div>
+      <div className={classes.menuszcze}>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/Page1'>Page1</Link>
+          <Link to='/Page2'>Page2</Link>
+        </div>
+      </div><div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Page1' element={<Page1 />} />
+          <Route path='/Page2' element={<Page2 />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route></Route>
-      </Routes>
-    </div>
+      </div>
   )
 }
