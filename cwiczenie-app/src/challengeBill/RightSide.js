370670604d85544b1a167643850bd6c4a68af6e6
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-18 20:14:37 +0200

    changes

--- cwiczenie-app/src/challengeBill/RightSide.js
+++ cwiczenie-app/src/challengeBill/RightSide.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from '../style/StyleBill.module.css'
 
 export const RightSide = () => {
   return (
-    <div>
+    <div className={classes.rightSide}>
       <div>
         <div>
           <h6>Tip Amount</h6>
@@ -12,6 +13,16 @@ export const RightSide = () => {
           <h2>$ 0.00</h2>
         </div>
       </div>
+      <div>
+        <div>
+          <h6>Total</h6>
+          <p>/person</p>
+        </div>
+        <div>
+          <h2>$ 0.00</h2>
+        </div>
+      </div>
+      <button>RESET</button>
     </div>
   )
 }
