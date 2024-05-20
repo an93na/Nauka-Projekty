3a8996e1b89a3db27bf23f2423bac303e221211e
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-20 19:54:27 +0200

    changes

--- cwiczenie-app/src/challengeBill/BillTotal.js
+++ cwiczenie-app/src/challengeBill/BillTotal.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from '../style/StyleBill.module.css'
 import { LeftSide } from './LeftSide'
 import { RightSide } from './RightSide'
 
 export const BillTotal = () => {
+  const [bill, setBill] = useState(0);
+  const [procent, setProcent] = useState(0);
+  const [numberOfPeople, setNumberOfPeople] = useState(0)
   return (
     <article className={classes.home}>
         <div className={classes.billWhite}>
