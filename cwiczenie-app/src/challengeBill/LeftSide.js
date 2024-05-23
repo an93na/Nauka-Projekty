00dcebc2d32684222e6fb574c9dc4e362be0f2cf
Anna Giermakowska <an.giermakowska@gmail.com>
2024-05-23 20:49:56 +0200

    changes

--- cwiczenie-app/src/challengeBill/LeftSide.js
+++ cwiczenie-app/src/challengeBill/LeftSide.js
@@ -3,7 +3,8 @@ import { BillEle } from './BillEle'
 import { SelectTip } from './SelectTip'
 import { NumberOfPeople } from './NumberOfPeople'
 
-export const LeftSide = () => {
+export const LeftSide = (props) => {
+  
   return (
     <div>
       <BillEle />
