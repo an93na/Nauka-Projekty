64cbe59d47a31234ab93bb75174755ec49f3b888
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-02 20:27:26 +0200

    changes

--- cwiczenie-app/src/challengeBill/NumberOfPeople.js
+++ cwiczenie-app/src/challengeBill/NumberOfPeople.js
@@ -2,13 +2,14 @@ import React from 'react'
 import person from '../asets/person.png'
 import classes from '../style/StyleBill.module.css'
 
-export const NumberOfPeople = () => {
+export const NumberOfPeople = (props) => {
+  const {numberOfPeople, setNumberOfPeople} = props
   return (
     <div>
       <h6>NumberOfPeople</h6>
       <div className={classes.rightKolum} style={{ padding: '5px' }}>
         <img src={person} alt="person" className={classes.imgPerson} />
-        <p>0</p>
+        <input type="text" value={numberOfPeople} onChange={(e) => setNumberOfPeople(e.target.value)}/>
       </div>
     </div>
   )
