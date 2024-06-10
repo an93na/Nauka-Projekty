bab3c4cdd496141eeb7c71824a3c706779b210ea
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-10 20:19:23 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -4,7 +4,7 @@ import classes from '../style/StyleBill.module.css';
 export const SelectTip = (props) => {
   const { procent, setProcent } = props;
   const [wlasna, setWlasna] = useState(false);
-  
+
   return (
     <div className={classes.billEle}>
       <h6>Select Tip %</h6>
@@ -17,7 +17,7 @@ export const SelectTip = (props) => {
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
         {wlasna ? (
-          <input type='text' className={classes.inputCust} />
+          <input type='text' className={classes.inputCust} style={{ width: '120px', height: '60px', margin: '0px' }} />
         ) : (
           <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
         )}
