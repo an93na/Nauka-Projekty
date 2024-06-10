9b6b0a6e75184f92923fe9da173ddc6a218d4965
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-10 20:15:11 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import classes from '../style/StyleBill.module.css'
+import React, { useState } from 'react';
+import classes from '../style/StyleBill.module.css';
 
 export const SelectTip = (props) => {
-  const { procent, setProcent } = props
-  const [wlasna, setWlasna] = useState(false)
-  console.log(wlasna)
+  const { procent, setProcent } = props;
+  const [wlasna, setWlasna] = useState(false);
+  
   return (
     <div className={classes.billEle}>
       <h6>Select Tip %</h6>
@@ -16,8 +16,12 @@ export const SelectTip = (props) => {
       <div className={classes.divSel}>
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
-        {wlasna ? <input type='text' className={classes.inputCust} /> : <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>}
+        {wlasna ? (
+          <input type='text' className={classes.inputCust} />
+        ) : (
+          <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
