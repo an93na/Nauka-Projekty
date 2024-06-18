39d6b4b1071fd2b507d5ba208afc41e4ff593564
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-18 20:04:21 +0200

    changes

--- cwiczenie-app/src/challengeBill/NumberOfPeople.js
+++ cwiczenie-app/src/challengeBill/NumberOfPeople.js
@@ -5,7 +5,7 @@ import classes from '../style/StyleBill.module.css'
 export const NumberOfPeople = (props) => {
   const { numberOfPeople, setNumberOfPeople } = props
   return (
-    <div>
+    <div style={{margin: '10px'}}>
       <h6>NumberOfPeople</h6>
       <div className={classes.kolumnPerson}>
         <img src={person} alt="person" className={classes.imgPerson} />
