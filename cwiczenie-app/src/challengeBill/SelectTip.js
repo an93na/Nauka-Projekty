1afa6480fda57201bcd306a8664d24d27f5461bd
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-19 19:56:45 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -4,6 +4,7 @@ import classes from '../style/StyleBill.module.css';
 export const SelectTip = (props) => {
   const { procent, setProcent } = props;
   const [wlasna, setWlasna] = useState(false);
+  const [custProcent, setCustPocent] = useState(0)
 
   return (
     <div className={classes.billEle}>
@@ -17,7 +18,7 @@ export const SelectTip = (props) => {
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
         {wlasna ? (
-          <input type='text' style={{ width: '120px', height: '60px', margin: '0px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)'}} value={procent} onChange={(e) => setProcent(e.target.value)}/>
+          <input type='text' style={{ width: '120px', height: '60px', margin: '0px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} value={custProcent} onChange={(e) => setCustPocent(e.target.value)} />
         ) : (
           <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
         )}
