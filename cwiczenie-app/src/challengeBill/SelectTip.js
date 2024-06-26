b9d58a6732bc7aee7b5557232c32f48663532f9a
Anna Giermakowska <an.giermakowska@gmail.com>
2024-06-26 22:28:37 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -26,7 +26,7 @@ export const SelectTip = (props) => {
 
   console.log(wpisanaWlasna)
   return (
-    <form className={classes.billEle}>
+    <div className={classes.billEle}>
       <h6>Select Tip %</h6>
       <div className={classes.divSel}>
         <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
@@ -37,12 +37,14 @@ export const SelectTip = (props) => {
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
         {wlasna ? (
-          <input type='text' style={{ width: '120px', height: '60px', margin: '0px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} value={procent} onChange={handleCustomInputChange} />
+          <form>
+            <input type='text' style={{ width: '120px', height: '60px', margin: '0px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} value={procent} onChange={handleCustomInputChange} />
+            <button type='submit'>zatwierdź procent</button>
+          </form>
         ) : (
           <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
         )}
       </div>
-      <button type='submit'>zatwierdź procent</button>
-    </form>
+    </div>
   );
 };
