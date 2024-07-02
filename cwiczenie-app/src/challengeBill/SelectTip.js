65a47860af1122cd00bb71fed3cce4ffc5573dcf
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-02 20:34:58 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -34,7 +34,22 @@ export const SelectTip = (props) => {
   return (
     <div className={classes.billEle}>
       <h6>Select Tip %</h6>
+      {/* <div className={classes.divSel}>
+        <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
+        <button className={classes.buttonSel} onClick={() => setProcent(0.1)}>10%</button>
+        <button className={classes.buttonSel} onClick={() => setProcent(0.15)}>15%</button>
+      </div>
       <div className={classes.divSel}>
+        <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
+        <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button> */}
+        {wlasna ? (
+          <form>
+            <input type='text' style={{ width: '120px', height: '60px', margin: '5px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} placeholder="wartość"value={custmValue} onChange={handleCustomInputChange} />
+            <button type='submit' style={{width: '150px', height: '60px', margin: '5px'}} onClick={submitCustomValue}>zatwierdź</button>
+          </form>
+        ) : (
+          <>
+          <div className={classes.divSel}>
         <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.1)}>10%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.15)}>15%</button>
@@ -42,15 +57,10 @@ export const SelectTip = (props) => {
       <div className={classes.divSel}>
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
-        {wlasna ? (
-          <form>
-            <input type='text' style={{ width: '120px', height: '60px', margin: '0px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} value={custmValue} onChange={handleCustomInputChange} />
-            <button type='submit' style={{width: '120px', height: '60px', margin: '0px'}} onClick={submitCustomValue}>zatwierdź</button>
-          </form>
-        ) : (
           <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
+          </div></>
         )}
       </div>
-    </div>
+    // </div>
   );
 };
