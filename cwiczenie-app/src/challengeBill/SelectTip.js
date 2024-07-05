01bd17ab84d2233d7067a40e3e2b0c6b27a1b095
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-05 21:44:40 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -16,7 +16,7 @@ export const SelectTip = (props) => {
       setWpisanaWlasna(true)
     }
   }
-  
+
   const handleCustomInputChange = (e) => {
     if (!isNaN(e.target.value)) {
       setCustmValue(e.target.value);
@@ -42,25 +42,25 @@ export const SelectTip = (props) => {
       <div className={classes.divSel}>
         <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button> */}
-        {wlasna ? (
-          <form>
-            <input type='text' style={{ width: '120px', height: '60px', margin: '5px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} placeholder="wartość"value={custmValue} onChange={handleCustomInputChange} />
-            <button type='submit' style={{width: '150px', height: '60px', margin: '5px'}} onClick={submitCustomValue}>zatwierdź</button>
-          </form>
-        ) : (
-          <>
+      {wlasna ? (
+        <form>
+          <input type='text' style={{ width: '120px', height: '60px', margin: '5px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} placeholder="wartość" value={custmValue} onChange={handleCustomInputChange} />
+          <button type='submit' style={{ width: '150px', height: '60px', margin: '5px' }} onClick={submitCustomValue}>zatwierdź</button>
+        </form>
+      ) : (
+        <>
           <div className={classes.divSel}>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.1)}>10%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.15)}>15%</button>
-      </div>
-      <div className={classes.divSel}>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
-          <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
+            <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
+            <button className={classes.buttonSel} onClick={() => setProcent(0.1)}>10%</button>
+            <button className={classes.buttonSel} onClick={() => setProcent(0.15)}>15%</button>
+          </div>
+          <div className={classes.divSel}>
+            <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
+            <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button>
+            <button className={classes.buttonCus} onClick={() => setWlasna(true)}>Custom</button>
           </div></>
-        )}
-      </div>
+      )}
+    </div>
     // </div>
   );
 };
