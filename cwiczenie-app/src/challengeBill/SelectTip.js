1aebade69e55c56df80df07096fd8ba4cf401d67
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-07 21:33:54 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -34,18 +34,9 @@ export const SelectTip = (props) => {
     setWlasna(false)
   }
 
-  console.log(wpisanaWlasna)
   return (
     <div className={classes.billEle}>
       <h6>Select Tip %</h6>
-      {/* <div className={classes.divSel}>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.05)}>5%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.1)}>10%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.15)}>15%</button>
-      </div>
-      <div className={classes.divSel}>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.25)}>25%</button>
-        <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button> */}
       {wlasna ? (
         <form>
           <button className={classes.buttonSel} onClick={resetTiphandle}>Wróć</button>
