807f5d21c7c4530dfdc405eddc68f4db5cd29276
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-07 21:33:11 +0200

    changes

--- cwiczenie-app/src/challengeBill/SelectTip.js
+++ cwiczenie-app/src/challengeBill/SelectTip.js
@@ -29,6 +29,10 @@ export const SelectTip = (props) => {
     setProcent(zamienWpisanaWartoscNaProcent(custmValue));
   }
 
+  const resetTiphandle = (e) => {
+    e.preventDefault()
+    setWlasna(false)
+  }
 
   console.log(wpisanaWlasna)
   return (
@@ -44,7 +48,7 @@ export const SelectTip = (props) => {
         <button className={classes.buttonSel} onClick={() => setProcent(0.5)}>50%</button> */}
       {wlasna ? (
         <form>
-          <button className={classes.buttonSel} onClick={(e)=> {e.preventDefault()}}>Wróć</button>
+          <button className={classes.buttonSel} onClick={resetTiphandle}>Wróć</button>
           <input type='text' style={{ width: '120px', height: '60px', margin: '5px', backgroundColor: 'rgb(235, 235, 235)', border: '1px solid rgb(204, 198, 198)', color: 'rgb(48, 45, 45)' }} placeholder="wartość" value={custmValue} onChange={handleCustomInputChange} />
           <button type='submit' className={classes.buttonSel} style={{ width: '150px', height: '60px', margin: '5px', border: 'none' }} onClick={submitCustomValue}>zatwierdź</button>
         </form>
