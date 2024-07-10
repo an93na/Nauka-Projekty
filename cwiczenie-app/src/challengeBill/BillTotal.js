6a019fccd6d7ebb032b19e6aa9f07c1af521ea50
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-10 21:58:01 +0200

    changes

--- cwiczenie-app/src/challengeBill/BillTotal.js
+++ cwiczenie-app/src/challengeBill/BillTotal.js
@@ -8,11 +8,13 @@ export const BillTotal = () => {
   const [procent, setProcent] = useState(0);
   const [numberOfPeople, setNumberOfPeople] = useState("");
   const [wlasna, setWlasna] = useState(false);
+  const [custmValue, setCustmValue] = useState('');
+
   return (
     <article className={classes.home}>
       <div className={classes.billWhite}>
-        <div><LeftSide numberOfPeople={numberOfPeople} setNumberOfPeople={setNumberOfPeople} procent={procent} setProcent={setProcent} bill={bill} setBill={setBill} wlasna={wlasna} setWlasna={setWlasna} /></div>
-        <div><RightSide numberOfPeople={numberOfPeople} procent={procent} bill={bill} setNumberOfPeople={setNumberOfPeople} setProcent={setProcent} setBill={setBill} wlasna={wlasna} setWlasna={setWlasna} /></div>
+        <div><LeftSide numberOfPeople={numberOfPeople} setNumberOfPeople={setNumberOfPeople} procent={procent} setProcent={setProcent} bill={bill} setBill={setBill} wlasna={wlasna} setWlasna={setWlasna} custmValue={custmValue} setCustmValue={setCustmValue} /></div>
+        <div><RightSide numberOfPeople={numberOfPeople} procent={procent} bill={bill} setNumberOfPeople={setNumberOfPeople} setProcent={setProcent} setBill={setBill} wlasna={wlasna} setWlasna={setWlasna} custmValue={custmValue} setCustmValue={setCustmValue} /></div>
       </div>
     </article>
   )
