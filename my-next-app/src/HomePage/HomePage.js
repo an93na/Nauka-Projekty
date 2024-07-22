4a1bdeead85662adf86d142ec9b9ae379aaf0edf
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-22 20:35:16 +0200

    changes

--- my-next-app/src/HomePage/HomePage.js
+++ my-next-app/src/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import { Desserts } from '../Desserts/Desserts'
 export const HomePage = () => {
   return (
     <article>
-      <div style={{display: 'flex', flexDirection: 'row', }}>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '10px' }}>
         <Desserts/>
         <Basket/>
       </div>
