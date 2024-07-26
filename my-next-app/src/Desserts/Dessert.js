77cec3d5925f111978463e8e9d032d6fb7e25b12
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-26 20:31:53 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -3,13 +3,12 @@ import React from 'react'
 export const Dessert = (props) => {
   const { tekst, setTekst, tekst2, setTekst2, cena, setCena } = props
   return (
-    <div>
+    <div style={{padding: '10px'}}>
       <img src="" alt="" />
       <button>Add to Card</button>
       <p>{tekst}</p>
       <h5>{tekst2}</h5>
       <h5>{cena}</h5>
-
     </div>
   )
 }
--- my-next-app/src/Desserts/Desserts.js
+++ my-next-app/src/Desserts/Desserts.js
@@ -7,7 +7,7 @@ export const Desserts = () => {
       <h3>
         Desserts
       </h3>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column'}}>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '10px' }}>
           <Dessert />
           <Dessert />
