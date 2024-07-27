52de5aad5f8e87965ac97800f4d0b5fda62f037a
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-27 17:34:49 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export const Dessert = (props) => {
-  const { tekst, setTekst, tekst2, setTekst2, cena, setCena } = props
+  const { tekst, tekst2, cena } = props
   return (
-    <div style={{padding: '10px'}}>
+    <div style={{ padding: '10px' }}>
       <img src="" alt="" />
       <button>Add to Card</button>
       <p>{tekst}</p>
       <h5>{tekst2}</h5>
-      <h5>{cena}</h5>
+      <h5 style={{ color: 'red' }}>{cena}</h5>
     </div>
   )
 }
