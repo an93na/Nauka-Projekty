8902cf0e7a1adb1c635f7eb2596d992a880615e2
Anna Giermakowska <an.giermakowska@gmail.com>
2024-07-29 22:35:00 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import cake from '../assets/cake.jpg'
 
 export const Dessert = (props) => {
   const { tekst, tekst2, cena } = props
   return (
     <div style={{ padding: '10px' }}>
-      <img src="" alt="" />
+      <img src={cake} alt="cake" />
       <button>Add to Card</button>
       <p>{tekst}</p>
       <h5>{tekst2}</h5>
