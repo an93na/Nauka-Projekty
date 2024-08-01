30db0c3bc37d68eb2be5f91c4234e61db8f487f0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-01 21:16:04 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -3,12 +3,12 @@ import React from 'react'
 export const Dessert = (props) => {
   const { tekst, tekst2, cena, cake } = props
   return (
-    <div style={{ padding: '10px', display: 'flex', flexDirection: 'column' }}>
+    <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <img src={cake} alt="cake" style={{width: '230px', height: '230px', borderRadius: '10px'}}/>
       <button style={{width: '180px', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray'}}>Add to Card</button>
-      <p>{tekst}</p>
-      <h5>{tekst2}</h5>
-      <h5 style={{ color: 'red' }}>{cena}</h5>
+      <p style={{marginBottom: '0px'}}>{tekst}</p>
+      <h5 style={{marginTop: '5px', marginBottom: '0px'}}>{tekst2}</h5>
+      <h5 style={{ color: 'red', marginTop: '5px' }}>{cena}</h5>
     </div>
   )
 }
