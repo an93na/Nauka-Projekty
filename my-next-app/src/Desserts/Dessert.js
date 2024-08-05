2b1811c4e32e94a596f881e33ad9524c9cc73448
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-05 22:35:33 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -4,7 +4,7 @@ export const Dessert = (props) => {
   const { tekst, tekst2, cena, cake } = props
   return (
     <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
-      <img src={cake} alt="cake" style={{width: '100%', height: '230px', borderRadius: '10px'}}/>
+      <img src={cake} alt="cake" style={{width: '100%', height: '80%', borderRadius: '10px'}}/>
       <button style={{width: '70%', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray', position: 'absolute', bottom: '35%'}}>Add to Card</button>
       <p style={{marginBottom: '0px', marginTop: '45px'}}>{tekst}</p>
       <h5 style={{marginTop: '5px', marginBottom: '0px'}}>{tekst2}</h5>
