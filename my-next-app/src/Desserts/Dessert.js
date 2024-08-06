f3bee9e0820fc061db96fd1b5f2455cf3c44d707
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-06 13:46:20 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -5,10 +5,10 @@ export const Dessert = (props) => {
   const { tekst, tekst2, cena, cake } = props
   return (
     <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
-      <img src={cake} alt="cake" style={{width: '100%', height: '80%', borderRadius: '10px'}}/>
-      <button style={{width: '70%', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray', position: 'absolute', bottom: '35%'}}><img src={shopping} alt="icon-basket" style={{width: '10%' , height: '5%'}} />Add to Card</button>
-      <p style={{marginBottom: '0px', marginTop: '45px'}}>{tekst}</p>
-      <h5 style={{marginTop: '5px', marginBottom: '0px'}}>{tekst2}</h5>
+      <img src={cake} alt="cake" style={{ width: '100%', height: '80%', borderRadius: '10px' }} />
+      <button style={{ width: '70%', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray', position: 'absolute', bottom: '35%' }}><img src={shopping} alt="icon-basket" style={{ width: '12%', height: '10%', padding: '1px' }} />Add to Card</button>
+      <p style={{ marginBottom: '0px', marginTop: '45px' }}>{tekst}</p>
+      <h5 style={{ marginTop: '5px', marginBottom: '0px' }}>{tekst2}</h5>
       <h5 style={{ color: 'red', marginTop: '5px' }}>{cena}</h5>
     </div>
   )
