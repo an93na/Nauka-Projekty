b9127b2693c3e55378e73a10243c133337ec7ea0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-07 17:18:32 +0200

    changes

---
+++ my-next-app/src/slice/DessertSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const DessertSlice = createSlice({
+    initialState: {},
+    name: 'dessert',
+    reducers: {}
+})
+
+export default DessertSlice.reducer
