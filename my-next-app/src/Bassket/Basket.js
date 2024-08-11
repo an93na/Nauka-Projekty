f8d7d2dff828df05d98b338a1ec1de4d16d70588
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-11 22:16:06 +0200

    changes

--- my-next-app/src/Bassket/Basket.js
+++ my-next-app/src/Bassket/Basket.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { selectDessert } from '../slice/DessertSlice'
+import { selectDessert, selectIsAnyInBasket } from '../slice/DessertSlice'
 import { useSelector } from 'react-redux'
 
 export const Basket = () => {
   const numberOfProducts = useSelector(selectDessert)
+  const isAnyInBasket = useSelector(selectIsAnyInBasket)
   return (
     <div>
       <h3>
         Basket
       </h3>
       <p>{numberOfProducts}</p>
+      <p>{isAnyInBasket}</p>
     </div>
   )
 }
