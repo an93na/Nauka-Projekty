6be86db233457f075ec19bd0cc42292b50c52e4c
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-12 20:31:06 +0200

    changes

--- my-next-app/src/Desserts/Dessert.js
+++ my-next-app/src/Desserts/Dessert.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import shopping from '../assets/shopping.png'
-import { addToBasket } from '../slice/DessertSlice'
+import { addToBasket, notEmptyBasket } from '../slice/DessertSlice'
 import { useDispatch } from 'react-redux'
 
 export const Dessert = (props) => {
@@ -9,14 +9,15 @@ export const Dessert = (props) => {
 
   const addToCardFunkcjon = () => {
     dispatch(addToBasket())
+    dispatch(notEmptyBasket())
   }
   return (
     <div style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
       <img src={cake} alt="cake" style={{ width: '100%', height: '80%', borderRadius: '10px' }} />
-      <button onClick={addToCardFunkcjon} 
-      style={{ width: '70%', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray', position: 'absolute', bottom: '35%' }}>
-      <img src={shopping} alt="icon-basket" style={{ width: '12%', height: '10%', padding: '1px' }} />
-      Add to Card
+      <button onClick={addToCardFunkcjon}
+        style={{ width: '70%', borderRadius: '50px', color: 'gray', backgroundColor: 'white', fontWeight: 'bold', border: '1px solid gray', position: 'absolute', bottom: '35%' }}>
+        <img src={shopping} alt="icon-basket" style={{ width: '12%', height: '10%', padding: '1px' }} />
+        Add to Card
       </button>
       <p style={{ marginBottom: '0px', marginTop: '45px' }}>{tekst}</p>
       <h5 style={{ marginTop: '5px', marginBottom: '0px' }}>{tekst2}</h5>
