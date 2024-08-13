d6b9a2d204d4db2ae44327f8a46bf3f6055eb68d
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-13 21:26:55 +0200

    changes

--- my-next-app/src/Bassket/Basket.js
+++ my-next-app/src/Bassket/Basket.js
@@ -10,7 +10,9 @@ export const Basket = () => {
       <h3>
         Basket
       </h3>
+      <div style={{backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px'}}>
       <p>{numberOfProducts}</p>
+      </div>
       {isAnyInBasket ? <p>tak</p>: <></>}
     </div>
   )
