dcf8ce9f9f5b25688c3baaa10d17523c46f97b01
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-20 21:28:42 +0200

    changes

--- my-next-app/src/Bassket/Basket.js
+++ my-next-app/src/Bassket/Basket.js
@@ -6,11 +6,11 @@ export const Basket = () => {
   const numberOfProducts = useSelector(selectDessert)
   const isAnyInBasket = useSelector(selectIsAnyInBasket)
   return (
-    <div>
+    <div style={{position:'relative'}}>
       <h3>
         Basket
       </h3>
-      <div style={{backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px'}}>
+      <div style={{backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px', position: 'absolute'}}>
       <p>{numberOfProducts}</p>
       </div>
       {isAnyInBasket ? <p>tak</p>: <></>}
