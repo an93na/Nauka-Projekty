b7d75e6e5e9d183c190b95c913d8ae3e82f05a7e
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-22 20:32:49 +0200

    changes

--- my-next-app/src/Bassket/Basket.js
+++ my-next-app/src/Bassket/Basket.js
@@ -7,11 +7,13 @@ export const Basket = () => {
   const numberOfProducts = useSelector(selectDessert)
   const isAnyInBasket = useSelector(selectIsAnyInBasket)
   return (
-    <div style={{ position: 'relative' }}>
+    <div>
+      <button style={{position: 'relative'}}>
       <img src={basketIcon} alt="koszyk" />
-      <div style={{ backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px', position: 'absolute', left: '40%', top: '0%' }}>
+      <div style={{ backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px', position: 'absolute', left: '50%', top: '30%' }}>
         <p style={{ fontSize: '10px', textAlign: 'center', fontWeight: 'bolder' }}>{numberOfProducts}</p>
       </div>
+      </button>
       {isAnyInBasket ? <p>tak</p> : <></>}
     </div>
   )
