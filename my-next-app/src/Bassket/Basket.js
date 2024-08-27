9332346ce1aafe56193f177423371bf959e1565d
Anna Giermakowska <an.giermakowska@gmail.com>
2024-08-27 20:30:20 +0200

    changes

--- my-next-app/src/Bassket/Basket.js
+++ my-next-app/src/Bassket/Basket.js
@@ -6,10 +6,13 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 export const Basket = () => {
   const numberOfProducts = useSelector(selectDessert)
   const isAnyInBasket = useSelector(selectIsAnyInBasket)
+  const onCliceed = () => {
+    console.log('klik')
+  }
   return (
     <div>
       <div style={{ position: 'relative' }}>
-        <ShoppingCartIcon style={{ color: 'white', margin: '0 10px', cursor: 'pointer' }} onClick={() => { }} />
+        <ShoppingCartIcon style={{ color: 'white', margin: '0 10px', cursor: 'pointer' }} onClick = {onCliceed}/>
         <div style={{ backgroundColor: 'red', borderRadius: '50%', width: '15px', height: '15px', position: 'absolute', left: '50%', top: '10%' }}>
           <p style={{ fontSize: '10px', textAlign: 'center', fontWeight: 'bolder' }}>{numberOfProducts}</p>
         </div>
