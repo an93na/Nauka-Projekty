4fd2f49aa1e8c358cb2b00f7fea6e2ecf0f3aea0
Anna Giermakowska <an.giermakowska@gmail.com>
2024-10-27 20:41:15 +0100

    changes

--- my-next-app/src/Bassket/Podsumowanie.js
+++ my-next-app/src/Bassket/Podsumowanie.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { selectPrice, selectProductsInBasket } from '../slice/DessertSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const Podsumowanie = () => {
+  const products = useSelector(selectProductsInBasket);
+  const kosztCalkowity = useSelector(selectPrice)
+
+  const podsumowanieZakupow = kosztCalkowity.toFixed(2);
+
   return (
-    <div>Podsumowanie</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>product name</th>
+            <th>price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) =>
+            <tr key={product.id}>
+              <td>{product.quantity}x</td>
+              <td><img src={product.cake} alt={product.id} style={{ width: '50%', height: '50%', borderRadius: '10px' }} /></td>
+              <td>{product.tekst}</td>
+              <td>{product.cena}</td>
+            </tr>
+          )}
+          <tr>
+            <td></td>
+            <td>Suma:</td>
+            <td></td>
+            <td><h6 style={{ margin: 1 }}>{podsumowanieZakupow}zł</h6></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
