793e8c7ca9010c07c46c6a879e27c42ec35518c9
Anna Giermakowska <an.giermakowska@gmail.com>
2024-11-03 20:30:24 +0100

    changes

--- my-next-app/src/Bassket/Podsumowanie.js
+++ my-next-app/src/Bassket/Podsumowanie.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { selectPrice, selectProductsInBasket } from '../slice/DessertSlice'
 import { useSelector } from 'react-redux'
+import { Button } from 'react-bootstrap';
 
 export const Podsumowanie = () => {
   const products = useSelector(selectProductsInBasket);
   const kosztCalkowity = useSelector(selectPrice)
 
   const podsumowanieZakupow = kosztCalkowity.toFixed(2);
+  const produktCena = (a, b) => (a * b).toFixed(2)
 
   return (
     <div>
       <table>
         <thead>
           <tr>
+            <th></th>
             <th></th>
             <th></th>
             <th>product name</th>
@@ -23,16 +26,27 @@ export const Podsumowanie = () => {
           {products.map((product) =>
             <tr key={product.id}>
               <td>{product.quantity}x</td>
+              <td>{product.cena}</td>
               <td><img src={product.cake} alt={product.id} style={{ width: '50%', height: '50%', borderRadius: '10px' }} /></td>
               <td>{product.tekst}</td>
-              <td>{product.cena}</td>
+              <td>{<td>{produktCena(product.cena, product.quantity)}zł</td>}</td>
             </tr>
           )}
           <tr>
             <td></td>
+            <td></td>
             <td>Suma:</td>
             <td></td>
             <td><h6 style={{ margin: 1 }}>{podsumowanieZakupow}zł</h6></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td>
+              <Button variant='danger'>Zatwierdź</Button>
+            </td>
+            <td></td>
+            <td></td>
           </tr>
         </tbody>
       </table>
