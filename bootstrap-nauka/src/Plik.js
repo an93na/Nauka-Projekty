c6fc18cb3a2f073539304c53b513d2b310ba3d8e
Anna Giermakowska <an.giermakowska@gmail.com>
2024-11-26 20:34:58 +0100

    changes

--- bootstrap-nauka/src/Plik.js
+++ bootstrap-nauka/src/Plik.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export const Plik = () => {
   return (
-    <div>
-      <h3>Bootstrap nauka</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">Lewa kolumna</div>
+        <div class="col-md-6">Prawa kolumna</div>
     </div>
+</div>
+
   )
 }
