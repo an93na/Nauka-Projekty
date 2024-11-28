d005ad80d1a9f7ebf8204a7f926fdeeb0923ebc3
Anna Giermakowska <an.giermakowska@gmail.com>
2024-11-28 20:42:51 +0100

    changes

--- cwiczenie-app/README.md
+++
@@ -1,6 +0,0 @@
-zadanie pochodzi z portalu frontendmentor:
-https://www.frontendmentor.io
-
-ćwiczenie 
-https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX
-
--- cwiczenie-app/src/components/Krzyzyk.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from '../style/Style.module.css'
-
-export const Krzyzyk = (props) => {
-    const { setIsOpen } = props
-    return (
-        <div className={classes.krzyzyk} onClick={() => { setIsOpen(prev => !prev) }}></div>
-    )
-}
