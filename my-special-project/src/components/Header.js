51199f0bce25670238f54e4ee921f3d5373493e9
Anna Giermakowska <an.giermakowska@gmail.com>
2024-12-02 19:19:05 +0100

    changes

--- my-special-project/src/components/Header.js
+++ my-special-project/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import headerStyle from '../styles/Header.module.css'
 
 export const Header = () => {
     return (
         <header>
-            <nav style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+            <nav className={headerStyle.navi}>
                 <div><p>1</p></div>
                 <div><p>2</p></div>
                 <div><p>3</p></div>
