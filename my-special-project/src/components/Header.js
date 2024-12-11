7bf8c6eef36f460d0902bf5e3f4385f361c01203
Anna Giermakowska <an.giermakowska@gmail.com>
2024-12-11 19:33:06 +0100

    changes

--- my-special-project/src/components/Header.js
+++ my-special-project/src/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import headerStyle from '../styles/Header.module.css'
 import linkStyle from '../styles/Link.module.css'
+import logo from '../assets/Frame 4d.png'
 
 
 export const Header = () => {
     return (
         <header>
             <nav className={headerStyle.navi}>
+                <img src={logo} alt="logo" className={headerStyle.logo}/>
                 <div><Link to="/" className={linkStyle.linksty}>Home</Link></div>
                 <div><Link to="/projects" className={linkStyle.linksty}>Projects</Link></div>
                 <div><Link to="/contact" className={linkStyle.linksty}>Contact</Link></div>
